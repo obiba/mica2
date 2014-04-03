e55cda981d763dfd469cc46f2751898211e98d9d
Cedric Thiebault <cthiebault@p3g.org>
2014-04-03 10:18:49 -0400

    Merge reader & writer

--- mica-rest/src/main/java/org/obiba/mica/web/rest/provider/protobuf/ProtobufJsonProvider.java
+++ mica-rest/src/main/java/org/obiba/mica/web/rest/provider/protobuf/ProtobufJsonProvider.java
@@ -10,38 +10,66 @@
 package org.obiba.mica.web.rest.provider.protobuf;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
 
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyReader;
 import javax.ws.rs.ext.MessageBodyWriter;
 import javax.ws.rs.ext.Provider;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 
 import com.google.common.base.Charsets;
+import com.google.protobuf.ExtensionRegistry;
 import com.google.protobuf.Message;
+import com.google.protobuf.Message.Builder;
 import com.googlecode.protobuf.format.JsonFormat;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
-@Component
 @Provider
+@Consumes(APPLICATION_JSON)
 @Produces(APPLICATION_JSON)
-public class ProtobufJsonWriterProvider implements MessageBodyWriter<Object> {
+public class ProtobufJsonProvider implements MessageBodyReader<Object>, MessageBodyWriter<Object> {
 
-  private static final Logger log = LoggerFactory.getLogger(ProtobufJsonWriterProvider.class);
+  private static final Logger log = LoggerFactory.getLogger(ProtobufJsonProvider.class);
 
   @Inject
   protected ProtobufProviderHelper helper;
+
+  @Override
+  public boolean isReadable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+    return Message.class.isAssignableFrom(type) || helper.isWrapped(type, genericType);
+  }
+
+  @Override
+  public Object readFrom(Class<Object> type, Type genericType, Annotation[] annotations, MediaType mediaType,
+      MultivaluedMap<String, String> httpHeaders, InputStream entityStream)
+      throws IOException, WebApplicationException {
+    Class<Message> messageType = helper.extractMessageType(type, genericType);
+
+    ExtensionRegistry extensionRegistry = helper.extensions().forMessage(messageType);
+    Builder builder = helper.builders().forMessage(messageType);
+
+    Readable input = new InputStreamReader(entityStream, Charsets.UTF_8);
+    if(helper.isWrapped(type, genericType)) {
+      // JsonFormat does not provide a mergeCollection method
+      return JsonIoUtil.mergeCollection(input, extensionRegistry, builder);
+    }
+    JsonFormat.merge(input, extensionRegistry, builder);
+    return builder.build();
+  }
 
   @Override
   public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
@@ -71,5 +99,4 @@ public class ProtobufJsonWriterProvider implements MessageBodyWriter<Object> {
       output.flush();
     }
   }
-
 }
--- mica-rest/src/main/java/org/obiba/mica/web/rest/provider/protobuf/ProtobufNativeProvider.java
+++ mica-rest/src/main/java/org/obiba/mica/web/rest/provider/protobuf/ProtobufNativeProvider.java
@@ -11,32 +11,33 @@ package org.obiba.mica.web.rest.provider.protobuf;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.MessageBodyReader;
+import javax.ws.rs.ext.MessageBodyWriter;
 import javax.ws.rs.ext.Provider;
 
-import org.springframework.stereotype.Component;
-
-import com.google.common.base.Charsets;
 import com.google.protobuf.ExtensionRegistry;
 import com.google.protobuf.Message;
 import com.google.protobuf.Message.Builder;
-import com.googlecode.protobuf.format.JsonFormat;
+import com.google.protobuf.MessageLite;
 
-import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
-
-@Component
 @Provider
-@Consumes(APPLICATION_JSON)
-public class ProtobufJsonReaderProvider implements MessageBodyReader<Object> {
+@Consumes(ProtobufNativeProvider.APPLICATION_X_PROTOBUF)
+@Produces(ProtobufNativeProvider.APPLICATION_X_PROTOBUF)
+public class ProtobufNativeProvider implements MessageBodyReader<Object>, MessageBodyWriter<Object> {
+
+  public static final String APPLICATION_X_PROTOBUF = "application/x-protobuf";
 
   @Inject
   protected ProtobufProviderHelper helper;
@@ -51,16 +52,41 @@ public class ProtobufJsonReaderProvider implements MessageBodyReader<Object> {
       MultivaluedMap<String, String> httpHeaders, InputStream entityStream)
       throws IOException, WebApplicationException {
     Class<Message> messageType = helper.extractMessageType(type, genericType);
-
     ExtensionRegistry extensionRegistry = helper.extensions().forMessage(messageType);
     Builder builder = helper.builders().forMessage(messageType);
-
-    Readable input = new InputStreamReader(entityStream, Charsets.UTF_8);
     if(helper.isWrapped(type, genericType)) {
-      // JsonFormat does not provide a mergeCollection method
-      return JsonIoUtil.mergeCollection(input, extensionRegistry, builder);
+      Collection<Message> msgs = new ArrayList<>();
+      Builder builderClone = builder.clone();
+      while(builderClone.mergeDelimitedFrom(entityStream, extensionRegistry)) {
+        msgs.add(builderClone.build());
+        builderClone = builder.clone();
+      }
+      return msgs;
     }
-    JsonFormat.merge(input, extensionRegistry, builder);
-    return builder.build();
+    return builder.mergeFrom(entityStream, extensionRegistry).build();
+  }
+
+  @Override
+  public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+    return Message.class.isAssignableFrom(type) || helper.isWrapped(type, genericType);
+  }
+
+  @Override
+  public long getSize(Object t, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+    return -1;
+  }
+
+  @Override
+  @SuppressWarnings({ "unchecked", "PMD.ExcessiveParameterList" })
+  public void writeTo(Object obj, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType,
+      MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream)
+      throws IOException, WebApplicationException {
+    if(helper.isWrapped(type, genericType)) {
+      for(MessageLite message : (Iterable<Message>) obj) {
+        message.writeDelimitedTo(entityStream);
+      }
+    } else {
+      ((MessageLite) obj).writeTo(entityStream);
+    }
   }
 }
