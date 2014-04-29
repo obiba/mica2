08e46385d4de4c45c63874a1a56ca572f0e049c4
Cedric Thiebault <cthiebault@p3g.org>
2014-04-29 11:01:12 -0400

    Use $log

--- mica-webapp/src/main/webapp/scripts/config/controller.js
+++ mica-webapp/src/main/webapp/scripts/config/controller.js
@@ -1,10 +1,10 @@
 'use strict';
 
-micaApp.controller('MicaConfigController', ['$scope', '$resource', '$modal', 'resolvedMicaConfig', 'MicaConfig',
+micaApp.controller('MicaConfigController', ['$scope', '$resource', '$modal', '$log', 'resolvedMicaConfig', 'MicaConfig',
 
-  function ($scope, $resource, $modal, resolvedMicaConfig, MicaConfig) {
+  function ($scope, $resource, $modal, $log, resolvedMicaConfig, MicaConfig) {
 
-    console.log($scope);
+    $log.debug('$scope:', $scope);
 
     $scope.micaConfig = resolvedMicaConfig;
     $scope.availableLanguages = $resource('ws/config/languages').get();
@@ -28,44 +28,43 @@ micaApp.controller('MicaConfigController', ['$scope', '$resource', '$modal', 're
 
   }]);
 
-var MicaConfigModalController = function ($scope, $modalInstance, MicaConfig, micaConfig, availableLanguages) {
+var MicaConfigModalController = function ($scope, $log, $modalInstance, MicaConfig, micaConfig, availableLanguages) {
 
   $scope.form = {};
 
-  console.log('MicaConfigModalController');
-  console.log($scope);
+  $log.debug('modal $scope:', $scope);
 
 
   $scope.micaConfig = micaConfig;
   $scope.availableLanguages = availableLanguages;
 
-  $scope.httpStatus = '';
-  $scope.errors = [];
-
-  $scope.processErrors = function (response) {
-    $scope.clearErrors();
-    $scope.httpStatus = response.status;
-    response.data.forEach(function (error) {
-      $scope.errors[error.path] = error.message;
-    });
-    console.log($scope.errors);
-  };
-
-  $scope.clearErrors = function () {
-    $scope.httpStatus = '';
-    $scope.errors = [];
-  };
+//  $scope.httpStatus = '';
+//  $scope.errors = [];
+//
+//  $scope.processErrors = function (response) {
+//    $scope.clearErrors();
+//    $scope.httpStatus = response.status;
+//    response.data.forEach(function (error) {
+//      $scope.errors[error.path] = error.message;
+//    });
+//    $log.debug('$scope.errors', $scope.errors);
+//  };
+//
+//  $scope.clearErrors = function () {
+//    $scope.httpStatus = '';
+//    $scope.errors = [];
+//  };
 
   $scope.save = function (form) {
-    console.log('$scope.form:');
-    console.log($scope.form);
+    $log.debug('$scope.form:', $scope.form);
     MicaConfig.save($scope.micaConfig,
       function () {
         $modalInstance.close();
       },
       function (response) {
-        console.log('ERROR:');
-        console.log(response);
+
+        $log.debug('begin save $scope:', $scope);
+        $log.debug('ERROR:', response);
 
 //        [{
 //          "message": "ne peut pas être vide",
@@ -74,31 +73,21 @@ var MicaConfigModalController = function ($scope, $modalInstance, MicaConfig, mi
 //          "invalidValue": ""
 //        }]
 
+        $scope.errors = [];
         response.data.forEach(function (error) {
-          console.log(error);
-          console.log('message: ' + error.message);
-          console.log('path: ' + error.path);
+          $log.debug('error: ', error);
 
           var field = error.path.substring(error.path.indexOf('.') + 1);
 
-          console.log('field: ' + field);
+          $log.debug('field: ', field);
 
-          $scope.form[field].$setValidity('server', false);
-          $scope.errors[field] = error.message.join(', ');
+          $scope.form.micaConfig.$setValidity('server', false);
+//          $scope.form.micaConfig[field].$setValidity('server', false);
+          $scope.errors[field] = error.message;
         });
 
-//        return angular.forEach(response.data, function(error) {
-//          console.log(error);
-//          console.log('message: ' + error.message);
-//          console.log('path: ' + error.path);
-//
-//          var field = error.path.substring(error.path.indexOf('.') + 1);
-//
-//          console.log('field: ' + field);
-//
-//          $scope.form[field].$setValidity('server', false);
-//          return $scope.errors[field] = error.message.join(', ');
-//        });
+        $log.debug('end save $scope:', $scope);
+
 
       });
   };
