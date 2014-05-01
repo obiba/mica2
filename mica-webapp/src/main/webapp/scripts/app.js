3fc46d5316d85584bcd41a8e298f7e1153512825
Cedric Thiebault <cthiebault@p3g.org>
2014-05-01 15:30:55 -0400

    Patch AngularJS core config to support dynamic input names

--- mica-webapp/src/main/webapp/scripts/app.js
+++ mica-webapp/src/main/webapp/scripts/app.js
@@ -109,6 +109,39 @@ micaApp
       tmhDynamicLocaleProvider.localeLocationPattern('bower_components/angular-i18n/angular-locale_{{locale}}.js')
       tmhDynamicLocaleProvider.useCookieStorage('NG_TRANSLATE_LANG_KEY');
     }])
+
+  // Workaround for bug #1404
+  // https://github.com/angular/angular.js/issues/1404
+  // Source: http://plnkr.co/edit/hSMzWC?p=preview
+  .config(['$provide', function ($provide) {
+    $provide.decorator('ngModelDirective', ['$delegate', function ($delegate) {
+      var ngModel = $delegate[0], controller = ngModel.controller;
+      ngModel.controller = ['$scope', '$element', '$attrs', '$injector', function (scope, element, attrs, $injector) {
+        var $interpolate = $injector.get('$interpolate');
+        attrs.$set('name', $interpolate(attrs.name || '')(scope));
+        $injector.invoke(controller, this, {
+          '$scope': scope,
+          '$element': element,
+          '$attrs': attrs
+        });
+      }];
+      return $delegate;
+    }]);
+    $provide.decorator('formDirective', ['$delegate', function ($delegate) {
+      var form = $delegate[0], controller = form.controller;
+      form.controller = ['$scope', '$element', '$attrs', '$injector', function (scope, element, attrs, $injector) {
+        var $interpolate = $injector.get('$interpolate');
+        attrs.$set('name', $interpolate(attrs.name || attrs.ngForm || '')(scope));
+        $injector.invoke(controller, this, {
+          '$scope': scope,
+          '$element': element,
+          '$attrs': attrs
+        });
+      }];
+      return $delegate;
+    }]);
+  }])
+
   .run(['$rootScope', '$location', '$http', 'AuthenticationSharedService', 'Session', 'USER_ROLES',
     function ($rootScope, $location, $http, AuthenticationSharedService, Session, USER_ROLES) {
       $rootScope.$on('$routeChangeStart', function (event, next) {
