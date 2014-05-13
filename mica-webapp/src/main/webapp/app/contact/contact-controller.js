a2dfa94b963cb52772ba39b536bd8a5ad0d79168
Cedric Thiebault <cthiebault@p3g.org>
2014-05-13 14:45:42 -0400

    Work on contact module

--- mica-webapp/src/main/webapp/app/contact/contact-controller.js
+++ mica-webapp/src/main/webapp/app/contact/contact-controller.js
@@ -1,60 +1,76 @@
 'use strict';
 
-mica.contact.controller('ContactViewController', ['$scope', '$resource', '$modal', '$log', 'contact', 'MicaConfigResource',
-  function ($scope, $resource, $modal, $log, contact, MicaConfigResource) {
+mica.contact
+  .controller('ContactController', ['$scope', '$modal', '$log',
+    function ($scope, $modal, $log) {
 
-    $log.debug('view contact', contact);
-
-    $scope.contact = contact;
-
-    $scope.edit = function () {
-
-      $modal
-        .open({
-          templateUrl: 'app/contact/contact-modal-form.html',
-          controller: ContactEditModalController,
+      $scope.viewContact = function (contact) {
+        $log.debug('viewContact', contact);
+        $scope.contact = contact;
+        $modal.open({
+          templateUrl: 'app/contact/contact-modal-view.html',
+          controller: 'ContactViewModalController',
           resolve: {
             contact: function () {
               return $scope.contact;
-            },
-            MicaConfigResource: MicaConfigResource
+            }
           }
-        })
-        .result.then(function (contact) {
-          $scope.contact = contact;
         });
-    };
+      };
 
-  }]);
+      $scope.editContact = function (contact) {
+        $log.debug('editContact', contact);
+        $scope.contact = contact;
+        $modal
+          .open({
+            templateUrl: 'app/contact/contact-modal-form.html',
+            controller: 'ContactEditModalController',
+            resolve: {
+              contact: function () {
+                return $scope.contact;
+              }
+            }
+          })
+          .result.then(function (contact) {
+            $scope.contact = contact;
+          });
+      }
 
-var ContactViewModalController = function ($scope, $modalInstance, contact) {
+    }])
+  .controller('ContactViewModalController', ['$scope', '$modalInstance', '$log', 'MicaConfigResource', 'contact',
+    function ($scope, $modalInstance, $log, MicaConfigResource, contact) {
 
-  $log.debug('view contact', contact);
+      $log.debug('view contact', contact);
 
-  $scope.contact = contact;
+      $scope.contact = contact;
 
-  $scope.close = function () {
-    $modalInstance.dismiss('close');
-  };
+      MicaConfigResource.get(function (micaConfig) {
+        $scope.languages = micaConfig.languages;
+      });
 
-};
+      $scope.close = function () {
+        $modalInstance.dismiss('close');
+      };
 
-var ContactEditModalController = function ($scope, $modalInstance, contact, MicaConfigResource) {
+    }])
+  .controller('ContactEditModalController', ['$scope', '$modalInstance', '$log', 'MicaConfigResource', 'contact',
+    function ($scope, $modalInstance, $log, MicaConfigResource, contact) {
 
-  $log.debug('edit contact', contact);
+      $log.debug('edit contact', contact);
 
-  $scope.contact = contact;
+      $scope.contact = contact;
 
-  MicaConfigResource.get(function (micaConfig) {
-    $scope.languages = micaConfig.languages;
-  });
+      MicaConfigResource.get(function (micaConfig) {
+        $scope.languages = micaConfig.languages;
+      });
 
-  $scope.save = function () {
-    $modalInstance.close($scope.contact);
-  };
+      $scope.save = function () {
+        $modalInstance.close($scope.contact);
+      };
 
-  $scope.cancel = function () {
-    $modalInstance.dismiss('cancel');
-  };
+      $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+      };
 
-};
+    }]);
+
