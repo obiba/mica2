9b79621a874e1886a7c69635dfef6ca5448b3d14
Cedric Thiebault <cthiebault@p3g.org>
2014-05-20 16:09:40 -0400

    Update swagger-ui

--- mica-webapp/Gruntfile.js
+++ mica-webapp/Gruntfile.js
@@ -71,13 +71,6 @@ module.exports = function (grunt) {
           port: 8080,
           https: false,
           changeOrigin: false
-        },
-        {
-          context: '/console',
-          host: 'localhost',
-          port: 8080,
-          https: false,
-          changeOrigin: false
         }
       ],
       options: {
