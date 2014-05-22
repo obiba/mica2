ce894ae3b7622d2d834d69ad4567f49af00fb653
Cedric Thiebault <cthiebault@p3g.org>
2014-05-22 16:20:50 -0400

    Add Http Error interceptor

--- mica-webapp/src/main/webapp/app/commons/rest/rest.js
+++ mica-webapp/src/main/webapp/app/commons/rest/rest.js
@@ -1,14 +1,15 @@
 'use strict';
 
-mica.rest = angular.module('mica.rest', [])
+mica.rest = angular.module('mica.rest', ['mica.notification'])
 
-  .config(function ($httpProvider) {
+  .config(['$httpProvider', function ($httpProvider) {
     $httpProvider.responseInterceptors.push('loadingHttpInterceptor');
+    $httpProvider.responseInterceptors.push('httpErrorsInterceptor');
     $httpProvider.defaults.transformRequest.push(function (data, headersGetter) {
       $('#loading').show();
       return data;
     });
-  })
+  }])
 
   // register the interceptor as a service, intercepts ALL angular ajax http calls
   .factory('loadingHttpInterceptor', ['$q', function ($q) {
@@ -23,4 +24,28 @@ mica.rest = angular.module('mica.rest', [])
           return $q.reject(response);
         });
     };
+  }])
+
+  .factory('httpErrorsInterceptor', ['$q', '$rootScope', '$log', function ($q, $rootScope, $log) {
+    return function (promise) {
+      return promise.then(
+        function (response) {
+          // $log.debug('httpErrorsInterceptor success', response);
+          return response;
+        },
+        function (response) {
+          $log.debug('httpErrorsInterceptor error', response);
+          var config = response.config;
+          if (config.errorHandler) {
+            return $q.reject(response);
+          }
+          $rootScope.$broadcast('showNotificationDialogEvent', {
+            iconClass: "fa-exclamation-triangle",
+            titleKey: 'error',
+            message: response.data ? response.data : angular.fromJson(response)
+          });
+          return $q.reject(response);
+        });
+    };
+
   }]);
