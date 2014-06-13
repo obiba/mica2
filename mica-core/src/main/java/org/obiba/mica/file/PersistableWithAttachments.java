6286a8329618f6cb882ff88ae746f1bfa5c09b78
Cedric Thiebault <cthiebault@p3g.org>
2014-06-13 10:42:14 -0400

    Delete AttachmentSerializer

--- mica-core/src/main/java/org/obiba/mica/file/PersistableWithAttachments.java
+++ mica-core/src/main/java/org/obiba/mica/file/PersistableWithAttachments.java
@@ -6,6 +6,8 @@ import org.springframework.data.domain.Persistable;
 
 public interface PersistableWithAttachments extends Persistable<String> {
 
+  Iterable<Attachment> getAllAttachments();
+
   @NotNull
   Attachment findAttachmentById(String attachmentId);
 
