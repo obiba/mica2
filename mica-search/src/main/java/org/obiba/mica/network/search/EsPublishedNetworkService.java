4558d0086c477bade54198de2514749d742023cb
Yannick Marcon <yannick.marcon@obiba.org>
2014-09-11 15:33:33 -0400

    MICASERVER-87 Published networks resource returns an object that wraps query results

--- mica-search/src/main/java/org/obiba/mica/network/search/EsPublishedNetworkService.java
+++ mica-search/src/main/java/org/obiba/mica/network/search/EsPublishedNetworkService.java
@@ -15,12 +15,23 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
+import javax.annotation.Nullable;
 import javax.inject.Inject;
 
+import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
+import org.obiba.mica.dataset.search.DatasetIndexer;
 import org.obiba.mica.network.domain.Network;
 import org.obiba.mica.network.service.PublishedNetworkService;
 import org.obiba.mica.search.AbstractPublishedDocumentService;
+import org.obiba.mica.web.model.Dtos;
+import org.obiba.mica.web.model.Mica;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -35,7 +46,49 @@ public class EsPublishedNetworkService  extends AbstractPublishedDocumentService
   private static final Logger log = LoggerFactory.getLogger(EsPublishedNetworkService.class);
 
   @Inject
+  private Client client;
+
+  @Inject
   private ObjectMapper objectMapper;
+
+  @Override
+  public Networks getNetworks(int from, int limit, @Nullable String sort, @Nullable String order,
+      @Nullable String studyId) {
+
+      QueryBuilder query = null;
+      if(studyId != null) {
+        query = QueryBuilders.termQuery("studyIds", studyId);
+      }
+
+      SearchRequestBuilder search = client.prepareSearch() //
+          .setIndices(NetworkIndexer.PUBLISHED_NETWORK_INDEX) //
+          .setTypes(NetworkIndexer.NETWORK_TYPE) //
+          .setQuery(query) //
+          .setFrom(from) //
+          .setSize(limit);
+
+      if(sort != null) {
+        search.addSort(
+            SortBuilders.fieldSort(sort).order(order == null ? SortOrder.ASC : SortOrder.valueOf(order.toUpperCase())));
+      }
+
+      log.info(search.toString());
+      SearchResponse response = search.execute().actionGet();
+      log.info(response.toString());
+
+      Networks networks = new Networks(Long.valueOf(response.getHits().getTotalHits()).intValue(), from, limit);
+
+      response.getHits().forEach(hit -> {
+        InputStream inputStream = new ByteArrayInputStream(hit.getSourceAsString().getBytes());
+        try {
+          networks.add(objectMapper.readValue(inputStream, Network.class));
+        } catch(IOException e) {
+          log.error("Failed retrieving a network", e);
+        }
+      });
+
+      return networks;
+    }
 
   @Override
   protected List<Network> processHits(SearchHits hits) {
