9db262beea3da71ec47889bc11ad2a06c427b275
Yannick Marcon <yannick.marcon@obiba.org>
2014-09-30 14:13:00 -0400

    Taxonomy web services added

---
+++ mica-core/src/main/java/org/obiba/mica/dataset/OpalService.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright (c) 2014 OBiBa. All rights reserved.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.obiba.mica.dataset;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+import javax.annotation.Nullable;
+import javax.inject.Inject;
+
+import org.obiba.magma.DatasourceFactory;
+import org.obiba.magma.support.Initialisables;
+import org.obiba.mica.micaConfig.MicaConfigService;
+import org.obiba.opal.core.cfg.NoSuchTaxonomyException;
+import org.obiba.opal.core.domain.taxonomy.Taxonomy;
+import org.obiba.opal.rest.client.magma.OpalJavaClient;
+import org.obiba.opal.rest.client.magma.RestDatasource;
+import org.obiba.opal.rest.client.magma.RestDatasourceFactory;
+import org.obiba.opal.web.model.Opal;
+import org.obiba.opal.web.taxonomy.Dtos;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.bind.RelaxedPropertyResolver;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OpalService implements EnvironmentAware {
+
+  private static final Logger log = LoggerFactory.getLogger(OpalService.class);
+
+  @Inject
+  private MicaConfigService micaConfigService;
+
+  private Map<String, RestDatasource> datasourceMap = new HashMap<>();
+
+  private RelaxedPropertyResolver opalPropertyResolver;
+
+  private OpalJavaClient opalJavaClient;
+
+  @Override
+  public void setEnvironment(Environment environment) {
+    opalPropertyResolver = new RelaxedPropertyResolver(environment, "opal.");
+  }
+
+  /**
+   * Get the datasource from the provided Opal server url.
+   *
+   * @param opalUrl if null, default Opal server url will be used.
+   * @param project
+   * @return
+   */
+  public synchronized RestDatasource getDatasource(@Nullable String opalUrl, String project) {
+    String baseUrl = opalUrl == null ? getDefaultOpal() : opalUrl;
+    while(baseUrl.endsWith("/")) {
+      baseUrl = baseUrl.substring(0, baseUrl.length() - 1);
+    }
+    String projectUrl = baseUrl + "/ws/datasource/" + project;
+    if(!datasourceMap.containsKey(projectUrl)) {
+      DatasourceFactory factory = new RestDatasourceFactory(project, baseUrl, getOpalUsername(), getOpalPassword(),
+          project);
+      RestDatasource datasource = (RestDatasource) factory.create();
+      Initialisables.initialise(datasource);
+      datasourceMap.put(projectUrl, datasource);
+    }
+    return datasourceMap.get(projectUrl);
+  }
+
+  /**
+   * Get a {@link org.obiba.opal.rest.client.magma.RestDatasource} from the default Opal server.
+   *
+   * @param project
+   * @return
+   */
+  public RestDatasource getDatasource(String project) {
+    return getDatasource(getDefaultOpal(), project);
+  }
+
+  /**
+   * Get the url of the default Opal server as defined in the configuration.
+   *
+   * @return
+   */
+  public String getDefaultOpal() {
+    return micaConfigService.getConfig().getOpal();
+  }
+
+  public Opal.TaxonomiesDto getTaxonomySummaryDtos() {
+    try {
+      OpalJavaClient opalClient = getOpalJavaClient();
+      URI uri = opalClient.newUri().segment("system","conf","taxonomies", "summaries").build();
+      return opalClient.getResource(Opal.TaxonomiesDto.class, uri, Opal.TaxonomiesDto.newBuilder());
+    } catch (URISyntaxException e) {
+      log.error("Malformed URI to Opal: " + getDefaultOpal(), e);
+      throw new NoSuchElementException();
+    }
+  }
+
+  /**
+   * Get the {@link org.obiba.opal.core.domain.taxonomy.Taxonomy} from Opal master.
+   *
+   * @param name
+   * @return
+   * @throws org.obiba.opal.core.cfg.NoSuchTaxonomyException
+   */
+  public Taxonomy getTaxonomy(String name) {
+    return Dtos.fromDto(getTaxonomyDto(name));
+  }
+
+  public Opal.TaxonomyDto getTaxonomyDto(String name) {
+    try {
+      OpalJavaClient opalClient = getOpalJavaClient();
+      URI uri = opalClient.newUri().segment("system","conf","taxonomy", name).build();
+      return opalClient.getResource(Opal.TaxonomyDto.class, uri, Opal.TaxonomyDto.newBuilder());
+    } catch (URISyntaxException e) {
+      log.error("Malformed URI to Opal: " + getDefaultOpal(), e);
+      throw new NoSuchTaxonomyException(name);
+    }
+  }
+
+  //
+  // Private methods
+  //
+
+  private String getOpalUsername() {
+    return opalPropertyResolver.getProperty("username");
+  }
+
+  private String getOpalPassword() {
+    return opalPropertyResolver.getProperty("password");
+  }
+
+  private OpalJavaClient getOpalJavaClient() throws URISyntaxException {
+    if (opalJavaClient != null) return opalJavaClient;
+
+    String opalUrl = getDefaultOpal();
+    while(opalUrl.endsWith("/")) {
+      opalUrl = opalUrl.substring(0, opalUrl.length() - 1);
+    }
+    if(!opalUrl.endsWith("/ws")) {
+      opalUrl = opalUrl + "/ws";
+    }
+    return opalJavaClient = new OpalJavaClient(opalUrl, getOpalUsername(), getOpalPassword());
+  }
+}
--- mica-rest/src/main/java/org/obiba/mica/micaConfig/rest/MicaConfigResource.java
+++ mica-rest/src/main/java/org/obiba/mica/micaConfig/rest/MicaConfigResource.java
@@ -9,11 +9,14 @@ import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
+import org.obiba.mica.dataset.OpalService;
 import org.obiba.mica.micaConfig.MicaConfigService;
 import org.obiba.mica.web.model.Dtos;
 import org.obiba.mica.web.model.Mica;
+import org.obiba.opal.web.model.Opal;
 
 import com.codahale.metrics.annotation.Timed;
 
@@ -22,6 +25,9 @@ public class MicaConfigResource {
 
   @Inject
   private MicaConfigService micaConfigService;
+
+  @Inject
+  private OpalService opalService;
 
   @Inject
   private Dtos dtos;
@@ -47,6 +53,18 @@ public class MicaConfigResource {
     Locale locale = Locale.ENGLISH;
     return Arrays.asList(Locale.getISOLanguages()).stream()
         .collect(Collectors.toMap(lang -> lang, lang -> new Locale(lang).getDisplayLanguage(locale)));
+  }
+
+  @GET
+  @Path("/taxonomies")
+  public Opal.TaxonomiesDto getTaxonomySummaries() {
+    return opalService.getTaxonomySummaryDtos();
+  }
+
+  @GET
+  @Path("/taxonomy/{name}")
+  public Opal.TaxonomyDto getTaxonomy(@PathParam("name") String name) {
+    return opalService.getTaxonomyDto(name);
   }
 
 }
