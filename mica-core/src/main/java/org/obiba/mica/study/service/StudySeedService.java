d66fe6bc51da1337802e30055dfe2f8a9fbed833
Yannick Marcon <yannick.marcon@obiba.org>
2014-10-28 08:42:45 -0400

    StudySeedService added.

---
+++ mica-core/src/main/java/org/obiba/mica/study/service/StudySeedService.java
@@ -0,0 +1,159 @@
+/*
+ * Copyright (c) 2014 OBiBa. All rights reserved.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.obiba.mica.study.service;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.annotation.Nullable;
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import org.obiba.mica.core.domain.LocalizedString;
+import org.obiba.mica.core.service.GitService;
+import org.obiba.mica.study.NoSuchStudyException;
+import org.obiba.mica.study.StudyStateRepository;
+import org.obiba.mica.study.domain.Study;
+import org.obiba.mica.study.domain.StudyState;
+import org.obiba.mica.study.event.DraftStudyUpdatedEvent;
+import org.obiba.mica.study.event.IndexStudiesEvent;
+import org.obiba.mica.study.event.StudyPublishedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Strings;
+import com.google.common.eventbus.EventBus;
+import com.google.common.io.Files;
+
+import static org.obiba.mica.core.domain.RevisionStatus.DRAFT;
+
+/**
+ * Import studies (save and publish) from JSON files found in MICA_SERVER_HOME/seed/in directory.
+ * The {@link org.obiba.mica.study.domain.Study} (or the list of Studies) should be deserializable by Jackson's
+ * {@link com.fasterxml.jackson.databind.ObjectMapper}.
+ */
+@Service
+@Validated
+public class StudySeedService {
+
+  private static final Logger log = LoggerFactory.getLogger(StudySeedService.class);
+
+  private static final String PATH_SEED = "${MICA_SERVER_HOME}/seed";
+
+  @Inject
+  private StudyService studyService;
+
+  @Inject
+  private ObjectMapper objectMapper;
+
+  private File seedRepository;
+
+  @PostConstruct
+  public void init() {
+    if(seedRepository == null && !Strings.isNullOrEmpty(System.getProperty("MICA_SERVER_HOME"))) {
+      seedRepository = new File(PATH_SEED.replace("${MICA_SERVER_HOME}", System.getProperty("MICA_SERVER_HOME")));
+    }
+  }
+
+  // Every 10s
+  @Scheduled(fixedDelay = 10 * 1000)
+  public void importSeed() {
+    if(seedRepository == null || !seedRepository.exists() || !seedRepository.isDirectory()) return;
+
+    File seedIn = new File(seedRepository, "in");
+    if(seedIn.exists() && seedIn.isDirectory()) {
+      Arrays.asList(seedIn.listFiles(pathname -> {
+        String name = pathname.getName().toLowerCase();
+        File lock = new File(pathname.getAbsolutePath() + ".lock");
+        return !lock.exists() && name.endsWith(".json") && name.startsWith("stud");
+      })).forEach(this::importSeed);
+    }
+  }
+
+  //
+  // Private methods
+  //
+
+  private void importSeed(File json) {
+    File lock = new File(json.getAbsolutePath() + ".lock");
+
+    try {
+      if(lock.exists() || !lock.createNewFile()) return;
+
+      if(json.getName().toLowerCase().startsWith("studies")) {
+        importStudies(json);
+      } else if(json.getName().toLowerCase().startsWith("study")) {
+        importStudy(json);
+      }
+
+    } catch(IOException e) {
+      log.error("Failed importing study seed: {}", json.getAbsolutePath(), e);
+    } finally {
+      if(lock.exists()) {
+        File out = new File(seedRepository, "out");
+        if(!out.exists()) out.mkdirs();
+        try {
+          Files.move(json, new File(out, json.getName()));
+          // release lock only if move succeeded
+          lock.delete();
+        } catch(IOException e) {
+          log.error("Failed moving seed file to: {}", out.getAbsolutePath());
+        }
+      }
+    }
+  }
+
+  /**
+   * Import Studies from a jackson file.
+   *
+   * @param json
+   * @throws java.io.IOException
+   */
+  private void importStudies(File json) throws IOException {
+    log.info("Seeding studies with file: {}", json.getAbsolutePath());
+    InputStream inputStream = new FileInputStream(json);
+    List<Study> studies = objectMapper.readValue(inputStream, new TypeReference<List<Study>>() {});
+    for(Study study : studies) {
+      studyService.save(study);
+      studyService.publish(study.getId());
+    }
+  }
+
+  /**
+   * Import a Study from a jackson file.
+   *
+   * @param json
+   */
+  private void importStudy(File json) throws IOException {
+    log.info("Seeding study with file: {}", json.getAbsolutePath());
+    InputStream inputStream = new FileInputStream(json);
+    Study study = objectMapper.readValue(inputStream, Study.class);
+    studyService.save(study);
+    studyService.publish(study.getId());
+  }
+
+}
--- mica-core/src/main/java/org/obiba/mica/study/service/StudyService.java
+++ mica-core/src/main/java/org/obiba/mica/study/service/StudyService.java
@@ -1,16 +1,9 @@
 package org.obiba.mica.study.service;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.annotation.Nullable;
-import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
@@ -24,22 +17,16 @@ import org.obiba.mica.study.domain.StudyState;
 import org.obiba.mica.study.event.DraftStudyUpdatedEvent;
 import org.obiba.mica.study.event.IndexStudiesEvent;
 import org.obiba.mica.study.event.StudyPublishedEvent;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Strings;
 import com.google.common.eventbus.EventBus;
-import com.google.common.io.Files;
 
 import static org.obiba.mica.core.domain.RevisionStatus.DRAFT;
 
@@ -47,9 +34,7 @@ import static org.obiba.mica.core.domain.RevisionStatus.DRAFT;
 @Validated
 public class StudyService implements ApplicationListener<ContextRefreshedEvent> {
 
-  private static final Logger log = LoggerFactory.getLogger(StudyService.class);
-
-  private static final String PATH_SEED = "${MICA_SERVER_HOME}/seed";
+  //private static final Logger log = LoggerFactory.getLogger(StudyService.class);
 
   @Inject
   private StudyStateRepository studyStateRepository;
@@ -62,15 +47,6 @@ public class StudyService implements ApplicationListener<ContextRefreshedEvent> 
 
   @Inject
   private ObjectMapper objectMapper;
-
-  private File seedRepository;
-
-  @PostConstruct
-  public void init() {
-    if(seedRepository == null && !Strings.isNullOrEmpty(System.getProperty("MICA_SERVER_HOME"))) {
-      seedRepository = new File(PATH_SEED.replace("${MICA_SERVER_HOME}", System.getProperty("MICA_SERVER_HOME")));
-    }
-  }
 
   @CacheEvict(value = "studies-draft", key = "#study.id")
   public void save(@NotNull @Valid Study study) {
@@ -179,82 +155,9 @@ public class StudyService implements ApplicationListener<ContextRefreshedEvent> 
 //    studyRepository.delete(id);
 //  }
 
-  // Every 10s
-  @Scheduled(fixedDelay = 10 * 1000)
-  public void importSeed() {
-    if(seedRepository == null || !seedRepository.exists() || !seedRepository.isDirectory()) return;
-
-    File seedIn = new File(seedRepository, "in");
-    if(seedIn.exists() && seedIn.isDirectory()) {
-      Arrays.asList(seedIn.listFiles(pathname -> {
-        String name = pathname.getName().toLowerCase();
-        File lock = new File(pathname.getAbsolutePath() + ".lock");
-        return !lock.exists() && name.endsWith(".json") && name.startsWith("stud");
-      })).forEach(this::importSeed);
-    }
-  }
-
   //
   // Private methods
   //
-
-  private void importSeed(File json) {
-    File lock = new File(json.getAbsolutePath() + ".lock");
-
-    try {
-      if(lock.exists() || !lock.createNewFile()) return;
-
-      if(json.getName().toLowerCase().startsWith("studies")) {
-        importStudies(json);
-      } else if(json.getName().toLowerCase().startsWith("study")) {
-        importStudy(json);
-      }
-
-    } catch(IOException e) {
-      log.error("Failed importing study seed: {}", json.getAbsolutePath(), e);
-    } finally {
-      if(lock.exists()) {
-        File out = new File(seedRepository, "out");
-        if(!out.exists()) out.mkdirs();
-        try {
-          Files.move(json, new File(out, json.getName()));
-          // release lock only if move succeeded
-          lock.delete();
-        } catch(IOException e) {
-          log.error("Failed moving seed file to: {}", out.getAbsolutePath());
-        }
-      }
-    }
-  }
-
-  /**
-   * Import Studies from a jackson file.
-   *
-   * @param json
-   * @throws IOException
-   */
-  private void importStudies(File json) throws IOException {
-    log.info("Seeding studies with file: {}", json.getAbsolutePath());
-    InputStream inputStream = new FileInputStream(json);
-    List<Study> studies = objectMapper.readValue(inputStream, new TypeReference<List<Study>>() {});
-    for(Study study : studies) {
-      save(study);
-      publish(study.getId());
-    }
-  }
-
-  /**
-   * Import a Study from a jackson file.
-   *
-   * @param json
-   */
-  private void importStudy(File json) throws IOException {
-    log.info("Seeding study with file: {}", json.getAbsolutePath());
-    InputStream inputStream = new FileInputStream(json);
-    Study study = objectMapper.readValue(inputStream, Study.class);
-    save(study);
-    publish(study.getId());
-  }
 
   @Nullable
   private String generateId(@NotNull Study study) {
