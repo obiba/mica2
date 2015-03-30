2b98746b909d2697f4ba44a4022075f89b30025c
Yannick Marcon <yannick.marcon@obiba.org>
2015-03-30 13:55:00 -0400

    MK-365 TRACE method requests receive a forbidden response

--- mica-webapp/src/main/java/org/obiba/mica/config/WebConfiguration.java
+++ mica-webapp/src/main/java/org/obiba/mica/config/WebConfiguration.java
@@ -1,15 +1,23 @@
 package org.obiba.mica.config;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.EnumSet;
 
 import javax.inject.Inject;
 import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
@@ -109,12 +117,11 @@ public class WebConfiguration implements ServletContextInitializer, JettyServerC
     };
     jettySsl.setWantClientAuth(true);
     jettySsl.setNeedClientAuth(false);
-    jettySsl.addExcludeProtocols("SSLv2","SSLv3");
+    jettySsl.addExcludeProtocols("SSLv2", "SSLv3");
 
     ServerConnector sslConnector = new ServerConnector(server, jettySsl);
     sslConnector.setPort(httpsPort);
     sslConnector.setIdleTimeout(MAX_IDLE_TIME);
-
 
     server.addConnector(sslConnector);
   }
@@ -123,6 +130,7 @@ public class WebConfiguration implements ServletContextInitializer, JettyServerC
   public void onStartup(ServletContext servletContext) throws ServletException {
     log.info("Web application configuration, using profiles: {}", Arrays.toString(env.getActiveProfiles()));
 
+    initAllowedMethods(servletContext);
     initAuthenticationFilter(servletContext);
 
     EnumSet<DispatcherType> disps = EnumSet.of(REQUEST, FORWARD, ASYNC);
@@ -138,20 +146,28 @@ public class WebConfiguration implements ServletContextInitializer, JettyServerC
     log.info("Web application fully configured");
   }
 
+  private void initAllowedMethods(ServletContext servletContext) {
+    log.debug("Registering Allowed Methods Filter");
+
+    FilterRegistration.Dynamic filterRegistration = servletContext.addFilter("noTrace", new NoTraceFilter());
+
+    filterRegistration.addMappingForUrlPatterns(EnumSet.of(REQUEST, FORWARD, ASYNC, INCLUDE, ERROR), true, "/*");
+    filterRegistration.setAsyncSupported(true);
+  }
+
   private void initAuthenticationFilter(ServletContext servletContext) {
     log.debug("Registering Authentication Filter");
     FilterRegistration.Dynamic filterRegistration = servletContext
       .addFilter("authenticationFilter", authenticationFilter);
 
-    if (filterRegistration == null) {
-      filterRegistration =
-        (FilterRegistration.Dynamic)servletContext.getFilterRegistration("authenticationFilter");
+    if(filterRegistration == null) {
+      filterRegistration = (FilterRegistration.Dynamic) servletContext.getFilterRegistration("authenticationFilter");
     }
 
     log.debug("Adding mappging to authentication filter registration");
 
-    filterRegistration.addMappingForUrlPatterns(
-      EnumSet.of(REQUEST, FORWARD, ASYNC, INCLUDE, ERROR), true, WS_ROOT + "/*");
+    filterRegistration
+      .addMappingForUrlPatterns(EnumSet.of(REQUEST, FORWARD, ASYNC, INCLUDE, ERROR), true, WS_ROOT + "/*");
     filterRegistration.setAsyncSupported(true);
   }
 
@@ -161,14 +177,14 @@ public class WebConfiguration implements ServletContextInitializer, JettyServerC
   private void initGzipFilter(ServletContext servletContext, EnumSet<DispatcherType> disps) {
     log.debug("Registering GZip Filter");
 
-    FilterRegistration.Dynamic compressingFilter = servletContext.addFilter("gzipFilter", new GzipFilter());
-    compressingFilter.addMappingForUrlPatterns(disps, true, "*.css");
-    compressingFilter.addMappingForUrlPatterns(disps, true, "*.json");
-    compressingFilter.addMappingForUrlPatterns(disps, true, "*.html");
-    compressingFilter.addMappingForUrlPatterns(disps, true, "*.js");
-    compressingFilter.addMappingForUrlPatterns(disps, true, "/metrics/*");
-    compressingFilter.addMappingForUrlPatterns(disps, true, WS_ROOT + "/*");
-    compressingFilter.setAsyncSupported(true);
+    FilterRegistration.Dynamic filterRegistration = servletContext.addFilter("gzipFilter", new GzipFilter());
+    filterRegistration.addMappingForUrlPatterns(disps, true, "*.css");
+    filterRegistration.addMappingForUrlPatterns(disps, true, "*.json");
+    filterRegistration.addMappingForUrlPatterns(disps, true, "*.html");
+    filterRegistration.addMappingForUrlPatterns(disps, true, "*.js");
+    filterRegistration.addMappingForUrlPatterns(disps, true, "/metrics/*");
+    filterRegistration.addMappingForUrlPatterns(disps, true, WS_ROOT + "/*");
+    filterRegistration.setAsyncSupported(true);
   }
 
   /**
@@ -229,4 +245,33 @@ public class WebConfiguration implements ServletContextInitializer, JettyServerC
     metricsAdminServlet.setLoadOnStartup(2);
   }
 
+  /**
+   * When a TRACE request is received, returns a Forbidden response.
+   */
+  private static class NoTraceFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
+      HttpServletRequest httpRequest = (HttpServletRequest) request;
+      HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+      if("TRACE".equals(httpRequest.getMethod())) {
+        httpResponse.reset();
+        httpResponse.sendError(HttpServletResponse.SC_FORBIDDEN, "TRACE method not allowed");
+        return;
+      }
+      chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+  }
 }
