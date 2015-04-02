caafc03168c039eb5105dc9d654d67e558c1a72e
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2015-04-02 19:40:07 -0400

    MK-357 - ignore studies with no population and DCEs with no endYear

--- mica-webapp/src/main/webapp/bower_components/mica-study-timeline/README.md
+++ mica-webapp/src/main/webapp/bower_components/mica-study-timeline/README.md
@@ -4,5 +4,10 @@
 
 1. Run `bower install` to install `d3` and `d3.chart` into `bower_components`
 1. Run `npm install -d` to install the grunt modules
-1. Run `grunt` to build the concatenated and minified versions in the project root. To run [jshint](http://www.jshint.com) continually on file changes in the `src` directory, run `grunt watch`
+1. Run `grunt` to build the concatenated and minified versions in the project root. To run [jshint](http://www.jshint.com) continually on file changes in the `src` directory, run `grunt watch`.
 
+## Usage
+
+```javascript
+new $.MicaTimeline(new $.StudyDtoParser()).create("#vis", getTestData()).addLegend();
+```
--- mica-webapp/src/main/webapp/bower_components/mica-study-timeline/src/d3-timeline.js
+++ mica-webapp/src/main/webapp/bower_components/mica-study-timeline/src/d3-timeline.js
@@ -6,25 +6,27 @@
     var DISPLAY_TYPES = ["circle", "rect"];
 
     var hover = function () {},
-        click = function () {},
-        scroll = function () {},
-        orient = "bottom",
-        width = null,
-        height = null,
-        tickFormat = { format: d3.format("d"),
-          tickTime: 1,
-          tickNumber: 1,
-          tickSize: 10 },
-        colorCycle = d3.scale.category20(),
-        display = "rect",
-        startYear = 0,
-        beginning = 0,
-        ending = 0,
-        margin = {left: 30, right: 30, top: 30, bottom: 30},
-        stacked = false,
-        rotateTicks = false,
-        itemHeight = 10,
-        itemMargin = 5;
+      click = function () {},
+      scroll = function () {},
+      orient = "bottom",
+      width = null,
+      height = null,
+      tickFormat = {
+        format: d3.format("d"),
+        tickTime: 1,
+        tickNumber: 1,
+        tickSize: 10
+      },
+      colorCycle = d3.scale.category20(),
+      display = "rect",
+      startYear = 0,
+      beginning = 0,
+      ending = 0,
+      margin = {left: 30, right: 30, top: 30, bottom: 30},
+      stacked = false,
+      rotateTicks = false,
+      itemHeight = 10,
+      itemMargin = 5;
 
     function timeline(gParent) {
       var g = gParent.append("g");
@@ -32,9 +34,9 @@
       var gParentItem = d3.select(gParent[0][0]);
 
       var yAxisMapping = {},
-          maxStack = 1,
-          minTime = 0,
-          maxTime = 0;
+        maxStack = 1,
+        minTime = 0,
+        maxTime = 0;
 
       setWidth();
 
@@ -79,22 +81,22 @@
 
       // draw the axis
       var xScale = d3.time.scale()
-          .domain([beginning, ending])
-          .range([margin.left, width - margin.right]);
+        .domain([beginning, ending])
+        .range([margin.left, width - margin.right]);
 
       var xAxis = d3.svg.axis()
-          .scale(xScale)
-          .orient(orient)
-          .tickFormat(formatByYear)
-          .tickSubdivide(1)
-          .tickValues(d3.range(beginning, ending + 1, 12))
-          .tickSize(tickFormat.tickSize, tickFormat.tickSize / 2, 0);
+        .scale(xScale)
+        .orient(orient)
+        .tickFormat(formatByYear)
+        .tickSubdivide(1)
+        .tickValues(d3.range(beginning, ending + 1, 12))
+        .tickSize(tickFormat.tickSize, tickFormat.tickSize / 2, 0);
 
       // draw axis
       g.append("g")
-          .attr("class", "axis")
-          .attr("transform", "translate(" + 0 + "," + (margin.top + (itemHeight + itemMargin) * maxStack) + ")")
-          .call(xAxis);
+        .attr("class", "axis")
+        .attr("transform", "translate(" + 0 + "," + (margin.top + (itemHeight + itemMargin) * maxStack) + ")")
+        .call(xAxis);
 
 
       // draw the chart
@@ -103,39 +105,40 @@
           var data = datum.population.events;
           var hasLabel = (typeof(datum.label) != "undefined");
           g.selectAll("svg").data(data).enter()
-              .append("path")
-              .attr("d", function drawRect(d, i) {
-                var rectX = getXPos(d, i);
-                var rectY = getStackPosition(d, i);
-                var rectWidth = getWidth(d, i);
-                return rightRoundedRect(rectX, rectY, rectWidth, itemHeight, 5);
-              })
-              .style("fill", datum.population.color)
-              .on("mouseover", function (d, i) {
-                hover(d, index, datum);
-              })
-              .on("click", function (d, i) {
-                click(d, index, datum);
-              })
-              .append("title").text(function (d) {
-                return d.title;
-              });
+            .append("path")
+            .attr('id', 'line-path')
+            .attr("d", function drawRect(d, i) {
+              var rectX = getXPos(d, i);
+              var rectY = getStackPosition(d, i);
+              var rectWidth = getWidth(d, i);
+              return rightRoundedRect(rectX, rectY, rectWidth, itemHeight, 5);
+            })
+            .style("fill", datum.population.color)
+            .on("mouseover", function (d, i) {
+              hover(d, index, datum);
+            })
+            .on("click", function (d, i) {
+              click(d, index, datum);
+            })
+            .append("title").text(function (d) {
+              return d.title;
+            });
 
           // add the label
           if (hasLabel) {
             gParent.append('text')
-                .attr("class", "timeline-label")
-                .attr("transform", "translate(" + 0 + "," + (itemHeight / 2 + margin.top + (itemHeight + itemMargin) * yAxisMapping[index]) + ")")
-                .text(hasLabel ? datum.label : datum.id);
+              .attr("class", "timeline-label")
+              .attr("transform", "translate(" + 0 + "," + (itemHeight / 2 + margin.top + (itemHeight + itemMargin) * yAxisMapping[index]) + ")")
+              .text(hasLabel ? datum.label : datum.id);
           }
 
           if (typeof(datum.icon) != "undefined") {
             gParent.append('image')
-                .attr("class", "timeline-label")
-                .attr("transform", "translate(" + 0 + "," + (margin.top + (itemHeight + itemMargin) * yAxisMapping[index]) + ")")
-                .attr("xlink:href", datum.icon)
-                .attr("width", margin.left)
-                .attr("height", itemHeight);
+              .attr("class", "timeline-label")
+              .attr("transform", "translate(" + 0 + "," + (margin.top + (itemHeight + itemMargin) * yAxisMapping[index]) + ")")
+              .attr("xlink:href", datum.icon)
+              .attr("width", margin.left)
+              .attr("height", itemHeight);
           }
 
           function getStackPosition(d, i) {
@@ -146,11 +149,11 @@
 
       if (rotateTicks) {
         g.selectAll("text")
-            .attr("transform", function (d) {
-              return "rotate(" + rotateTicks + ")translate(" +
-                  (this.getBBox().width / 2 + 10) + "," + // TODO: change this 10
-                  this.getBBox().height / 2 + ")";
-            });
+          .attr("transform", function (d) {
+            return "rotate(" + rotateTicks + ")translate(" +
+              (this.getBBox().width / 2 + 10) + "," + // TODO: change this 10
+              this.getBBox().height / 2 + ")";
+          });
       }
 
       var gSize = g[0][0].getBoundingClientRect();
@@ -198,12 +201,12 @@
 
       function rightRoundedRect(x, y, width, height, radius) {
         return "M" + x + "," + y +
-            "h" + (width - radius) +
-            "a" + radius + "," + radius + " 0 0 1 " + radius + "," + radius +
-            "v" + (height - 2 * radius) +
-            "a" + radius + "," + radius + " 0 0 1 " + -radius + "," + radius +
-            "h" + (radius - width) +
-            "z";
+          "h" + (width - radius) +
+          "a" + radius + "," + radius + " 0 0 1 " + radius + "," + radius +
+          "v" + (height - 2 * radius) +
+          "a" + radius + "," + radius + " 0 0 1 " + -radius + "," + radius +
+          "h" + (radius - width) +
+          "z";
       }
 
     }
