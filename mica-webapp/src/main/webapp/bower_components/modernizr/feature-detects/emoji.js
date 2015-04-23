ba44f0c885e3f9b31adadeba1b42e28d1c5379b2
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2015-04-23 13:35:42 -0400

    Used exact version number for bower and npm modules

--- mica-webapp/src/main/webapp/bower_components/modernizr/feature-detects/emoji.js
+++ mica-webapp/src/main/webapp/bower_components/modernizr/feature-detects/emoji.js
@@ -1,9 +1,9 @@
 // Requires a Modernizr build with `canvastext` included
 // http://www.modernizr.com/download/#-canvas-canvastext
-Modernizr.addTest('emoji', function () {
+Modernizr.addTest('emoji', function() {
   if (!Modernizr.canvastext) return false;
   var node = document.createElement('canvas'),
-    ctx = node.getContext('2d');
+      ctx = node.getContext('2d');
   ctx.textBaseline = 'top';
   ctx.font = '32px Arial';
   ctx.fillText('\ud83d\ude03', 0, 0); // "smiling face with open mouth" emoji
--- mica-webapp/src/main/webapp/bower_components/modernizr/feature-detects/forms-validation.js
+++ mica-webapp/src/main/webapp/bower_components/modernizr/feature-detects/forms-validation.js
@@ -4,36 +4,36 @@
 //    - Modernizr.inputtypes.numer && Modernizr.formvalidation (browser supports rangeOverflow, typeMismatch etc. for type=number)
 //    - Modernizr.input.required && Modernizr.formvalidation (browser supports valueMissing)
 //
-(function (document, Modernizr) {
+(function(document, Modernizr){
 
 
-  Modernizr.formvalidationapi = false;
-  Modernizr.formvalidationmessage = false;
+Modernizr.formvalidationapi = false;
+Modernizr.formvalidationmessage = false;
 
-  Modernizr.addTest('formvalidation', function () {
+Modernizr.addTest('formvalidation', function(){
     var form = document.createElement('form');
-    if (!('checkValidity' in form)) {
-      return false;
+    if ( !('checkValidity' in form) ) {
+        return false;
     }
     var body = document.body,
 
-      html = document.documentElement,
+    html = document.documentElement,
 
-      bodyFaked = false,
+    bodyFaked = false,
 
-      invaildFired = false,
+    invaildFired = false,
 
-      input;
+    input;
 
     Modernizr.formvalidationapi = true;
 
     // Prevent form from being submitted
-    form.onsubmit = function (e) {
-      //Opera does not validate form, if submit is prevented
-      if (!window.opera) {
-        e.preventDefault();
-      }
-      e.stopPropagation();
+    form.onsubmit = function(e) {
+        //Opera does not validate form, if submit is prevented
+        if ( !window.opera ) {
+            e.preventDefault();
+        }
+        e.stopPropagation();
     };
 
     // Calling form.submit() doesn't trigger interactive validation, 
@@ -47,23 +47,23 @@
     form.style.top = '-99999em';
 
     // We might in <head> in which case we need to create body manually
-    if (!body) {
-      bodyFaked = true;
-      body = document.createElement('body');
-      //avoid crashing IE8, if background image is used
-      body.style.background = "";
-      html.appendChild(body);
+    if ( !body ) {
+        bodyFaked = true;
+        body = document.createElement('body');
+        //avoid crashing IE8, if background image is used
+        body.style.background = "";
+        html.appendChild(body);
     }
 
     body.appendChild(form);
 
-    input = form.getElementsByTagName('input')[0];
+    input = form.getElementsByTagName('input')[0];	
 
     // Record whether "invalid" event is fired
-    input.oninvalid = function (e) {
-      invaildFired = true;
-      e.preventDefault();
-      e.stopPropagation();
+    input.oninvalid = function(e) {
+        invaildFired = true;
+        e.preventDefault();
+        e.stopPropagation();
     };
 
     //Opera does not fully support the validationMessage property
@@ -77,7 +77,7 @@
     bodyFaked && html.removeChild(body);
 
     return invaildFired;
-  });
+});
 
 
 })(document, window.Modernizr);
