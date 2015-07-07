f8413f3e3ed5f0ca9af2626f2f508bf8c67aa329
Yannick Marcon <yannick.marcon@obiba.org>
2015-07-07 17:32:31 -0400

    MK-505 More accurate combined statistics

--- mica-search/src/main/java/org/obiba/mica/dataset/search/rest/harmonized/CombinedStatistics.java
+++ mica-search/src/main/java/org/obiba/mica/dataset/search/rest/harmonized/CombinedStatistics.java
@@ -1,42 +1,38 @@
 package org.obiba.mica.dataset.search.rest.harmonized;
 
 import java.util.Collection;
-import java.util.Iterator;
+import java.util.List;
+import java.util.stream.IntStream;
 
 import org.obiba.mica.web.model.Mica;
 
+import com.google.common.collect.Lists;
+
 public class CombinedStatistics {
 
-  private Mica.StatisticsDto stats;
+  private final List<Mica.StatisticsDto> stats = Lists.newArrayList();
 
-  private int count = 0;
+  private final List<Integer> counts = Lists.newArrayList();
 
-  private Mica.StatisticsDto tableStats;
-
-  private int tableCount = 0;
-
-  CombinedStatistics(Mica.DatasetVariableAggregationsDto.Builder aggDto,
-    Mica.DatasetVariableAggregationDto tableAggDto) {
-    stats = Mica.StatisticsDto.newBuilder(aggDto.getStatistics()).build();
-    tableStats = tableAggDto.getStatistics();
-    count = aggDto.getN();
-    tableCount = tableAggDto.getN();
+  CombinedStatistics(Collection<Mica.DatasetVariableAggregationDto> aggDtos) {
+    aggDtos.stream().filter(Mica.DatasetVariableAggregationDto::hasStatistics).forEach(a -> {
+      stats.add(a.getStatistics());
+      counts.add(a.getN());
+    });
   }
 
-  CombinedStatistics(Mica.DatasetVariableAggregationDto.Builder aggDto,
-    Mica.DatasetVariableAggregationDto tableAggDto) {
-    stats = Mica.StatisticsDto.newBuilder(aggDto.getStatistics()).build();
-    tableStats = tableAggDto.getStatistics();
-    count = aggDto.getN();
-    tableCount = tableAggDto.getN();
+  public boolean hasStatistics() {
+    return !stats.isEmpty();
   }
 
   public int getCount() {
-    return count + tableCount;
+    return counts.stream().mapToInt(c -> c).sum();
   }
 
   public float getSum() {
-    return (stats.hasSum() ? stats.getSum() : 0) + (tableStats.hasSum() ? tableStats.getSum() : 0);
+    return Double
+      .valueOf(stats.stream().filter(Mica.StatisticsDto::hasSum).mapToDouble(Mica.StatisticsDto::getSum).sum())
+      .floatValue();
   }
 
   public boolean hasMean() {
@@ -48,172 +44,123 @@ public class CombinedStatistics {
   }
 
   public boolean hasMin() {
-    return tableStats.hasMin() && tableStats.getMin() != Float.POSITIVE_INFINITY;
+    for(Mica.StatisticsDto stat : stats) {
+      if(stat.hasMin() && stat.getMin() != Float.POSITIVE_INFINITY) return true;
+    }
+    return false;
   }
 
   public float getMin() {
-    return hasMin() ? stats.hasMin() ? Math.min(stats.getMin(), tableStats.getMin()) : tableStats.getMin() : 0;
+    return Double.valueOf(stats.stream().filter(s -> s.hasMin() && s.getMin() != Float.POSITIVE_INFINITY)
+      .mapToDouble(Mica.StatisticsDto::getMin).reduce(0, Math::min)).floatValue();
   }
 
   public boolean hasMax() {
-    return tableStats.hasMax() && tableStats.getMax() != Float.NEGATIVE_INFINITY;
+    for(Mica.StatisticsDto stat : stats) {
+      if(stat.hasMax() && stat.getMax() != Float.NEGATIVE_INFINITY) return true;
+    }
+    return false;
   }
 
   public float getMax() {
-    return hasMax() ? stats.hasMax() ? Math.max(stats.getMax(), tableStats.getMax()) : tableStats.getMax() : 0;
+    return Double.valueOf(stats.stream().filter(s -> s.hasMax() && s.getMax() != Float.NEGATIVE_INFINITY)
+      .mapToDouble(Mica.StatisticsDto::getMax).reduce(0, Math::max)).floatValue();
   }
 
   public boolean hasSumOfSquares() {
-    return tableStats.hasSumOfSquares();
+    for(Mica.StatisticsDto stat : stats) {
+      if(stat.hasSumOfSquares()) return true;
+    }
+    return false;
   }
 
   public float getSumOfSquares() {
-    return hasSumOfSquares()
-      ? (stats.hasSumOfSquares() ? 0 : stats.getSumOfSquares()) + tableStats.getSumOfSquares()
-      : 0;
+    return Double.valueOf(
+      stats.stream().filter(Mica.StatisticsDto::hasSumOfSquares).mapToDouble(Mica.StatisticsDto::getSumOfSquares).sum())
+      .floatValue();
   }
 
   public float getVariance() {
-    float mean = getMean();
+    int count = getCount();
 
     // ESSG = error sum of squares within each group = variance * (n-1)
     // ESS = error sum of squares = sum(var(i) * (n(i)-1))
-    float essg = (stats.hasVariance() ? stats.getVariance() : 0) * (count - 1);
-    float tableEssg = (tableStats.hasVariance() ? tableStats.getVariance() : 0) * (tableCount - 1);
-    float ess = essg + tableEssg;
+    float ess = Double.valueOf(IntStream.range(0, stats.size()).filter(i -> stats.get(i).hasVariance())
+      .mapToDouble(i -> stats.get(i).getVariance() * (counts.get(i) - 1)).sum()).floatValue();
 
-    // GM = grand mean = sum(n(i) * mean(i))
-    float tableMean = tableStats.hasMean() ? tableStats.getMean() : 0;
-    float gm = (mean * count + tableMean * tableCount) / getCount();
+    // GM = grand mean = sum(mean(i) * n(i))
+    float gm = Double.valueOf(IntStream.range(0, stats.size()).filter(i -> stats.get(i).hasMean())
+      .mapToDouble(i -> stats.get(i).getMean() * counts.get(i)).sum() / count).floatValue();
 
-    // GSS = group sum of squares = (mean(i) - gm)^2 * n(i)
-    float gss = Double.valueOf(Math.pow(mean - gm, 2)).floatValue() * count;
-    float tableGss = Double.valueOf(Math.pow(tableMean - gm, 2)).floatValue() * tableCount;
-    float tgss = gss + tableGss;
+    // GSS = group sum of squares = sum(mean(i) - gm)^2 * n(i)
+    float gss = Double.valueOf(IntStream.range(0, stats.size()).filter(i -> stats.get(i).hasMean())
+      .mapToDouble(i -> Math.pow(stats.get(i).getMean() - gm, 2) * counts.get(i)).sum()).floatValue();
 
     // GV = grand variance
-    return getCount() == 1 ? 0 : (ess - tgss) / (getCount() - 1);
+    return count == 1 ? 0 : (ess - gss) / (count - 1);
   }
 
   //
   // Static methods
   //
 
-  public static void mergeAggregations(Mica.DatasetVariableAggregationsDto.Builder aggDto,
-    Mica.DatasetVariableAggregationDto tableAggDto) {
-    mergeFrequencies(aggDto, tableAggDto);
-    mergeStatistics(aggDto, tableAggDto);
-    aggDto.setTotal(aggDto.getTotal() + tableAggDto.getTotal());
-    aggDto.setN(aggDto.getN() + tableAggDto.getN());
-  }
-
-  public static void mergeAggregations(Mica.DatasetVariableAggregationDto.Builder aggDto,
-    Mica.DatasetVariableAggregationDto tableAggDto) {
-    mergeFrequencies(aggDto, tableAggDto);
-    mergeStatistics(aggDto, tableAggDto);
-    aggDto.setTotal(aggDto.getTotal() + tableAggDto.getTotal());
-    aggDto.setN(aggDto.getN() + tableAggDto.getN());
-  }
-
-  public static void mergeFrequencies(Mica.DatasetVariableAggregationsDto.Builder aggDto,
-    Mica.DatasetVariableAggregationDto tableAggDto) {
-    if(tableAggDto.getFrequenciesCount() == 0) return;
-
-    for(Mica.FrequencyDto tableFreq : tableAggDto.getFrequenciesList()) {
-      boolean found = false;
-      for(int i = 0; i < aggDto.getFrequenciesCount(); i++) {
-        Mica.FrequencyDto freq = aggDto.getFrequencies(i);
-        if(freq.getValue().equals(tableFreq.getValue())) {
-          aggDto.setFrequencies(i, freq.toBuilder().setCount(freq.getCount() + tableFreq.getCount()).build());
-          found = true;
-          break;
-        }
-      }
-      if(!found) {
-        aggDto.addFrequencies(tableFreq.toBuilder());
-      }
-    }
-  }
-
-  public static void mergeFrequencies(Mica.DatasetVariableAggregationDto.Builder aggDto,
-    Mica.DatasetVariableAggregationDto tableAggDto) {
-    if(tableAggDto.getFrequenciesCount() == 0) return;
-
-    for(Mica.FrequencyDto tableFreq : tableAggDto.getFrequenciesList()) {
-      boolean found = false;
-      for(int i = 0; i < aggDto.getFrequenciesCount(); i++) {
-        Mica.FrequencyDto freq = aggDto.getFrequencies(i);
-        if(freq.getValue().equals(tableFreq.getValue())) {
-          aggDto.setFrequencies(i, freq.toBuilder().setCount(freq.getCount() + tableFreq.getCount()).build());
-          found = true;
-          break;
-        }
-      }
-      if(!found) {
-        aggDto.addFrequencies(tableFreq.toBuilder());
-      }
-    }
-  }
-
-  public static void mergeStatistics(Mica.DatasetVariableAggregationsDto.Builder aggDto,
-    Mica.DatasetVariableAggregationDto tableAggDto) {
-    if(!tableAggDto.hasStatistics()) return;
-
-    if(!aggDto.hasStatistics()) {
-      aggDto.setStatistics(tableAggDto.getStatistics().toBuilder());
-    } else {
-      CombinedStatistics combined = new CombinedStatistics(aggDto, tableAggDto);
-      if(combined.getCount() > 0) {
-        Mica.StatisticsDto.Builder builder = aggDto.getStatistics().toBuilder();
-
-        builder.setSum(combined.getSum());
-        if(combined.hasMean()) builder.setMean(combined.getMean());
-        if(combined.hasMin()) builder.setMin(combined.getMin());
-        if(combined.hasMax()) builder.setMax(combined.getMax());
-        if(combined.hasSumOfSquares()) builder.setSumOfSquares(combined.getSumOfSquares());
-
-        float gv = combined.getVariance();
-        builder.setVariance(gv);
-        builder.setStdDeviation(Double.valueOf(Math.pow(gv, 0.5)).floatValue());
-
-        aggDto.setStatistics(builder);
-      }
-    }
-  }
-
-  public static void mergeStatistics(Mica.DatasetVariableAggregationDto.Builder aggDto,
-    Mica.DatasetVariableAggregationDto tableAggDto) {
-    if(!tableAggDto.hasStatistics()) return;
-
-    if(!aggDto.hasStatistics()) {
-      aggDto.setStatistics(tableAggDto.getStatistics().toBuilder());
-    } else {
-      CombinedStatistics combined = new CombinedStatistics(aggDto, tableAggDto);
-      if(combined.getCount() > 0) {
-        Mica.StatisticsDto.Builder builder = aggDto.getStatistics().toBuilder();
-
-        builder.setSum(combined.getSum());
-        if(combined.hasMean()) builder.setMean(combined.getMean());
-        if(combined.hasMin()) builder.setMin(combined.getMin());
-        if(combined.hasMax()) builder.setMax(combined.getMax());
-        if(combined.hasSumOfSquares()) builder.setSumOfSquares(combined.getSumOfSquares());
-
-        float gv = combined.getVariance();
-        builder.setVariance(gv);
-        builder.setStdDeviation(Double.valueOf(Math.pow(gv, 0.5)).floatValue());
-
-        aggDto.setStatistics(builder);
-      }
-    }
-  }
-
   public static Mica.DatasetVariableAggregationDto mergeAggregations(
-    Collection<Mica.DatasetVariableAggregationDto> aggs) {
-    Iterator<Mica.DatasetVariableAggregationDto> iterator = aggs.iterator();
-    Mica.DatasetVariableAggregationDto.Builder start = iterator.next().toBuilder();
-    while(iterator.hasNext()) {
-      mergeAggregations(start, iterator.next());
-    }
-    return start.build();
+    Collection<Mica.DatasetVariableAggregationDto> aggDtos) {
+    Mica.DatasetVariableAggregationDto.Builder aggDto = Mica.DatasetVariableAggregationDto.newBuilder();
+    mergeAggregations(aggDto, aggDtos);
+    return aggDto.build();
+  }
+
+  //
+  // Private methods
+  //
+
+  private static void mergeAggregations(Mica.DatasetVariableAggregationDto.Builder aggDto,
+    Collection<Mica.DatasetVariableAggregationDto> aggDtos) {
+    mergeFrequencies(aggDto, aggDtos);
+    mergeStatistics(aggDto, aggDtos);
+    aggDto.setN(aggDtos.stream().mapToInt(Mica.DatasetVariableAggregationDto::getN).sum());
+    aggDto.setTotal(aggDtos.stream().mapToInt(Mica.DatasetVariableAggregationDto::getTotal).sum());
+    aggDtos.forEach(a -> {
+      if(a.hasTerm()) aggDto.setTerm(a.getTerm());
+    });
+  }
+
+  private static void mergeFrequencies(Mica.DatasetVariableAggregationDto.Builder aggDto,
+    Collection<Mica.DatasetVariableAggregationDto> aggDtos) {
+
+    aggDtos.stream().forEach(a -> a.getFrequenciesList().forEach(f -> {
+      boolean found = false;
+      for(int i = 0; i < aggDto.getFrequenciesCount(); i++) {
+        Mica.FrequencyDto freq = aggDto.getFrequencies(i);
+        if(freq.getValue().equals(f.getValue())) {
+          aggDto.setFrequencies(i, freq.toBuilder().setCount(freq.getCount() + f.getCount()).build());
+          found = true;
+          break;
+        }
+      }
+      if(!found) {
+        aggDto.addFrequencies(Mica.FrequencyDto.newBuilder(f));
+      }
+    }));
+  }
+
+  private static void mergeStatistics(Mica.DatasetVariableAggregationDto.Builder aggDto,
+    Collection<Mica.DatasetVariableAggregationDto> aggDtos) {
+
+    CombinedStatistics combined = new CombinedStatistics(aggDtos);
+    if(!combined.hasStatistics()) return;
+
+    Mica.StatisticsDto.Builder builder = Mica.StatisticsDto.newBuilder();
+    builder.setSum(combined.getSum());
+    if(combined.hasMean()) builder.setMean(combined.getMean());
+    if(combined.hasMin()) builder.setMin(combined.getMin());
+    if(combined.hasMax()) builder.setMax(combined.getMax());
+    if(combined.hasSumOfSquares()) builder.setSumOfSquares(combined.getSumOfSquares());
+
+    float gv = combined.getVariance();
+    builder.setVariance(gv);
+    builder.setStdDeviation(Double.valueOf(Math.pow(gv, 0.5)).floatValue());
+    aggDto.setStatistics(builder);
   }
 }
