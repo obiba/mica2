bb4f89071a8b0a8e19163f0b5c08321cc2e4281c
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2015-08-19 18:01:55 -0400

    Implemented REST APIs for DAO only to query about users of a given application and/or group.

--- mica-core/src/main/java/org/obiba/mica/user/UserProfileService.java
+++ mica-core/src/main/java/org/obiba/mica/user/UserProfileService.java
@@ -1,5 +1,11 @@
 package org.obiba.mica.user;
 
+import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.Nullable;
+import javax.validation.constraints.NotNull;
+
 import org.obiba.mica.core.service.AgateRestService;
 import org.obiba.shiro.realm.ObibaRealm.Subject;
 import org.slf4j.Logger;
@@ -10,9 +16,13 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
 
 @Service
 public class UserProfileService extends AgateRestService {
@@ -22,30 +32,83 @@ public class UserProfileService extends AgateRestService {
   public void init() {
     initInternal();
   }
-
+  
   @Cacheable(value="agate-subjects")
-  public synchronized Subject getProfile(String username) {
+  public synchronized Subject getProfile(@NotNull String username) {
+    Assert.notNull(username, "Username cannot be null");
+    Subject subject = getProfileInternal(getProfileServiceUrl(username));
+
+    if (subject == null) {
+      // return dummy Subject in case communication with Agate failed
+      subject = new Subject();
+      subject.setUsername(username);
+    }
+
+    return subject;
+  }
+
+  public synchronized Subject getProfileByApplication(@NotNull String username, @NotNull String application,
+    @Nullable String group) {
+    Assert.notNull(username, "Username cannot be null");
+    Assert.notNull(application, "Application name cannot be null");
+    return getProfileInternal(getProfileServiceUrlByApp(username, application, group));
+  }
+
+  public synchronized List<Subject> getProfilesByApplication(@NotNull String application,
+    @Nullable String group) {
+    Assert.notNull(application, "Application name cannot be null");
     try {
       RestTemplate template = newRestTemplate();
       HttpHeaders headers = new HttpHeaders();
       headers.set(APPLICATION_AUTH_HEADER, getApplicationAuth());
-      HttpEntity<String> entity = new HttpEntity<String>(null, headers);
-      ResponseEntity<Subject> response = template.exchange(getServiceUrl(username), HttpMethod.GET, entity, Subject.class);
+      HttpEntity<String> entity = new HttpEntity<>(null, headers);
+      ResponseEntity<Subject[]> response =
+        template.exchange(getProfileServiceUrlByApp(null, application, group), HttpMethod.GET, entity, Subject[].class);
+
+      return Arrays.asList(response.getBody());
+    } catch(RestClientException e) {
+      log.error("Agate connection failure: {}", e.getMessage(), e);
+    }
+
+    return Lists.newArrayList();
+  }
+
+  private synchronized Subject getProfileInternal(String serviceUrl) {
+    try {
+      RestTemplate template = newRestTemplate();
+      HttpHeaders headers = new HttpHeaders();
+      headers.set(APPLICATION_AUTH_HEADER, getApplicationAuth());
+      HttpEntity<String> entity = new HttpEntity<>(null, headers);
+      ResponseEntity<Subject> response = template.exchange(serviceUrl, HttpMethod.GET, entity, Subject.class);
+
       Subject subject = response.getBody();
       return subject;
     } catch(RestClientException e) {
       log.error("Agate connection failure: {}", e.getMessage(), e);
     }
 
-    // return dummy Subject in case communication with Agate failed
-    Subject subject = new Subject();
-    subject.setUsername(username);
-    return subject;
+    return null;
   }
 
-  private String getServiceUrl(String username) {
+  private String getProfileServiceUrl(String username) {
     return UriComponentsBuilder.fromHttpUrl(getAgateUrl()).path(DEFAULT_REST_PREFIX).path(
       String.format("/tickets/subject/%s", username)).build().toUriString();
+  }
+
+  private String getProfileServiceUrlByApp(String username, String application, String group) {
+    UriComponentsBuilder urlBuilder = UriComponentsBuilder.fromHttpUrl(getAgateUrl()).path(DEFAULT_REST_PREFIX);
+
+    if (Strings.isNullOrEmpty(username)) {
+      urlBuilder.path(String.format("/application/%s/users", application));
+    } else {
+      urlBuilder.path(String.format("/application/%s/user/%s", application, username));
+    }
+
+    if (!Strings.isNullOrEmpty(group)) {
+      urlBuilder.queryParam("group", group);
+    }
+
+    return urlBuilder.build().toUriString();
   }
 
 }
