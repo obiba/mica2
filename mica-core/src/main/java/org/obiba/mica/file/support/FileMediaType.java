338d7639d23ed3b86aa8b0cc0c3590e67a928468
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2016-04-12 16:20:20 -0400

    MK-812 - added end-point for downloading w/ Content-Disposition inline.

---
+++ mica-core/src/main/java/org/obiba/mica/file/support/FileMediaType.java
@@ -0,0 +1,138 @@
+package org.obiba.mica.file.support;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum FileMediaType {
+  AVI("avi"),
+  BMP("bmp"),
+  BZ2("bz2"),
+  CSS("css"),
+  DTD("dtd"),
+  DOC("doc"),
+  DOCX("docx"),
+  DOTX("dotx"),
+  ES("es"),
+  EXE("exe"),
+  GIF("gif"),
+  GZ("gz"),
+  HQX("hqx"),
+  HTML("html"),
+  JAR("jar"),
+  JPG("jpg"),
+  JS("js"),
+  MIDI("midi"),
+  MP3("mp3"),
+  MPEG("mpeg"),
+  OGG("ogg"),
+  PDF("pdf"),
+  PL("pl"),
+  PNG("png"),
+  POTX("potx"),
+  PPSX("ppsx"),
+  PPT("ppt"),
+  PPTX("pptx"),
+  PS("ps"),
+  QT("qt"),
+  RA("ra"),
+  RAM("ram"),
+  RDF("rdf"),
+  RTF("rtf"),
+  SGML("sgml"),
+  SIT("sit"),
+  SLDX("sldx"),
+  SVG("svg"),
+  SWF("swf"),
+  TAR_GZ("tar.gz"),
+  TGZ("tgz"),
+  TIFF("tiff"),
+  TSV("tsv"),
+  TXT("txt"),
+  WAV("wav"),
+  XLAM("xlam"),
+  XLS("xls"),
+  XLSB("xlsb"),
+  XLSX("xlsx"),
+  XLTX("xltx"),
+  XML("xml"),
+  ZIP("zip");
+
+  public String extension(FileMediaType mt) {
+    return mt.extension;
+  }
+
+  public String type() {
+    return type(this);
+  }
+
+  public static String type(FileMediaType mt) {
+    return type(mt.extension);
+  }
+
+  public static String type(String ext) {
+    return map.get(ext.replaceAll("/^\\./", ""));
+  }
+
+  private String extension;
+
+  FileMediaType(String ext) {
+    extension = ext;
+  }
+
+  private static Map<String, String> map = new HashMap<>();
+  static {
+    map.put("avi", "video/avi");
+    map.put("bmp", "image/bmp");
+    map.put("bz2", "application/x-bzip2");
+    map.put("css", "text/css");
+    map.put("dtd", "application/xml-dtd");
+    map.put("doc", "application/msword");
+    map.put("docx", "application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+    map.put("dotx", "application/vnd.openxmlformats-officedocument.wordprocessingml.template");
+    map.put("es", "application/ecmascript");
+    map.put("exe", "application/octet-stream");
+    map.put("gif", "image/gif");
+    map.put("gz", "application/x-gzip");
+    map.put("hqx", "application/mac-binhex40");
+    map.put("html", "text/html");
+    map.put("jar", "application/java-archive");
+    map.put("jpg", "image/jpeg");
+    map.put("js", "application/x-javascript");
+    map.put("midi", "audio/x-midi");
+    map.put("mp3", "audio/mpeg");
+    map.put("mpeg", "video/mpeg");
+    map.put("ogg", "application/ogg");
+    map.put("pdf", "application/pdf");
+    map.put("pl", "application/x-perl");
+    map.put("png", "image/png");
+    map.put("potx", "application/vnd.openxmlformats-officedocument.presentationml.template");
+    map.put("ppsx", "application/vnd.openxmlformats-officedocument.presentationml.slideshow");
+    map.put("ppt", "application/vnd.ms-powerpointtd>");
+    map.put("pptx", "application/vnd.openxmlformats-officedocument.presentationml.presentation");
+    map.put("ps", "application/postscript");
+    map.put("qt", "video/quicktime");
+    map.put("ra", "audio/x-pn-realaudio");
+    map.put("ram", "audio/x-pn-realaudio");
+    map.put("rdf", "application/rdf");
+    map.put("rtf", "application/rtf");
+    map.put("sgml", "text/sgml");
+    map.put("sit", "application/x-stuffit");
+    map.put("sldx", "application/vnd.openxmlformats-officedocument.presentationml.slide");
+    map.put("svg", "image/svg+xml");
+    map.put("swf", "application/x-shockwave-flash");
+    map.put("tar.gz", "application/x-tar");
+    map.put("tgz", "application/x-tar");
+    map.put("tiff", "image/tiff");
+    map.put("tsv", "text/tab-separated-values");
+    map.put("txt", "text/plain");
+    map.put("wav", "audio/wav");
+    map.put("xlam", "application/vnd.ms-excel.addin.macroEnabled.12");
+    map.put("xls", "application/vnd.ms-excel");
+    map.put("xlsb", "application/vnd.ms-excel.sheet.binary.macroEnabled.12");
+    map.put("xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+    map.put("xltx", "application/vnd.openxmlformats-officedocument.spreadsheetml.template");
+    map.put("xml", "application/xml");
+    map.put("zip", "application/zip");
+  }
+
+}
--- mica-rest/src/main/java/org/obiba/mica/file/rest/PublishedFileSystemResource.java
+++ mica-rest/src/main/java/org/obiba/mica/file/rest/PublishedFileSystemResource.java
@@ -1,18 +1,22 @@
 package org.obiba.mica.file.rest;
 
 import javax.inject.Inject;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.obiba.mica.file.Attachment;
 import org.obiba.mica.file.FileStoreService;
+import org.obiba.mica.file.support.FileMediaType;
 import org.obiba.mica.web.model.Mica;
 import org.springframework.stereotype.Component;
 
 import com.codahale.metrics.annotation.Timed;
+import com.google.common.io.Files;
 
 @Component
 @Path("/")
@@ -30,8 +34,18 @@ public class PublishedFileSystemResource extends AbstractFileSystemResource {
   @GET
   @Path("/file-dl/{path:.*}")
   @Timed
-  public Response downloadFile(@PathParam("path") String path) {
+  public Response downloadFile(@PathParam("path") String path,
+    @QueryParam("inline") @DefaultValue("false") boolean inline) {
     Attachment attachment = doGetAttachment(path);
+
+    if (inline) {
+      String filename = attachment.getName();
+      return Response.ok(fileStoreService.getFile(attachment.getFileReference()))
+        .header("Content-Disposition", "inline; filename=\"" + filename + "\"")
+        .type(FileMediaType.type(Files.getFileExtension(filename)))
+        .build();
+    }
+
     return Response.ok(fileStoreService.getFile(attachment.getFileReference()))
       .header("Content-Disposition", "attachment; filename=\"" + attachment.getName() + "\"").build();
   }
