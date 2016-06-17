270d6e60234de92494bb234f19850b2e4b2a0a2a
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2016-06-17 13:48:16 -0400

    Updated OBiBa's schema form add-on.

--- mica-webapp/src/main/webapp/bower_components/sf-localized-string/dist/sf-localized-string.js
+++ mica-webapp/src/main/webapp/bower_components/sf-localized-string/dist/sf-localized-string.js
@@ -1,76 +1,78 @@
-angular.module("sfLocalizedStringTemplates", []).run(["$templateCache", function($templateCache) {$templateCache.put("src/templates/sf-localized-string.html","<div class=\"form-group\"\n     ng-controller=\"LocalizedStringController\"\n     ng-class=\"{\'has-error\': form.disableErrorState !== true && hasError(), \'has-success\': form.disableSuccessState !== true && hasSuccess(), \'has-feedback\': form.feedback !== false }\"\n     schema-validate=\"form\" sf-field-model >\n  <!--<pre>{{form|json}}</pre>-->\n  <label class=\"control-label\" ng-show=\"showTitle()\">{{form.title}}</label>\n  <div class=\"pull-right dropdown\" ng-class=\"{\'open\': open}\" ng-if=\"form.locales && form.locales.length > 1\">\n    <a href class=\"dropdown-toggle\" ng-click=\"toggleDropdown()\">{{selectedLocale}} <span class=\"caret\"></span></a>\n    <ul class=\"dropdown-menu\">\n      <li ng-repeat=\"loc in form.locales\"><a href ng-click=\"selectLocale(loc)\">{{loc}}</a></li>\n    </ul>\n  </div>\n  <div ng-if=\"!form.rows || form.rows <= 1\"\n       ng-class=\"{\'form-group\' : !$last}\">\n    <input type=\"text\" class=\"form-control\"\n           sf-field-model=\"replaceAll\" ng-model=\"$$value$$[locale]\"\n           ng-repeat=\"locale in form.locales\"\n           ng-show=\"locale === selectedLocale\"/>\n  </div>\n  <div ng-if=\"form.rows && form.rows > 1\"\n       ng-class=\"{\'form-group\' : !$last\">\n    <textarea class=\"form-control\"\n              sf-field-model=\"replaceAll\" ng-model=\"$$value$$[locale]\"\n              rows=\"{{form.rows ? form.rows : 5}}\"\n              ng-repeat=\"locale in form.locales\"\n              ng-show=\"locale === selectedLocale\"></textarea>\n  </div>\n  <span class=\"help-block\" sf-message=\"form.description\"></span>\n</div>\n");}]);
+angular.module("sfLocalizedStringTemplates", []).run(["$templateCache", function($templateCache) {$templateCache.put("src/templates/sf-localized-string.html","<div class=\"form-group\"\n     ng-controller=\"LocalizedStringController\"\n     ng-class=\"{\'has-error\': form.disableErrorState !== true && hasError(), \'has-success\': form.disableSuccessState !== true && hasSuccess(), \'has-feedback\': form.feedback !== false }\"\n     schema-validate=\"form\" sf-field-model >\n  <!--<pre>{{form|json}}</pre>-->\n  <label class=\"control-label\" ng-show=\"showTitle()\">{{form.title}}</label>\n  <div class=\"pull-right dropdown\" ng-class=\"{\'open\': open}\" ng-if=\"form.locales && form.locales.length > 1\">\n    <a href class=\"dropdown-toggle\" ng-click=\"toggleDropdown()\">{{selectedLocale}} <span class=\"caret\"></span></a>\n    <ul class=\"dropdown-menu\">\n      <li ng-repeat=\"loc in form.locales\"><a href ng-click=\"selectLocale(loc)\">{{loc}}</a></li>\n    </ul>\n  </div>\n  <div ng-if=\"!form.rows || form.rows <= 1\"\n       ng-class=\"{\'form-group\' : !$last}\">\n    <input type=\"text\" class=\"form-control\"\n           sf-field-model=\"replaceAll\" ng-model=\"$$value$$[locale]\"\n           ng-repeat=\"locale in form.locales\"\n           ng-show=\"locale === selectedLocale\"/>\n  </div>\n  <div ng-if=\"form.rows && form.rows > 1\"\n       ng-class=\"{\'form-group\' : !$last}\">\n    <textarea class=\"form-control\"\n              sf-field-model=\"replaceAll\" ng-model=\"$$value$$[locale]\"\n              rows=\"{{form.rows ? form.rows : 5}}\"\n              ng-repeat=\"locale in form.locales\"\n              ng-show=\"locale === selectedLocale\"></textarea>\n  </div>\n  <span class=\"help-block\" sf-message=\"form.description\"></span>\n</div>\n");}]);
 angular.module('sfLocalizedString', [
   'schemaForm',
   'sfLocalizedStringTemplates'
 ]).config(['schemaFormProvider', 'schemaFormDecoratorsProvider', 'sfBuilderProvider', 'sfPathProvider',
-  function(schemaFormProvider,  schemaFormDecoratorsProvider, sfBuilderProvider, sfPathProvider) {
+    function(schemaFormProvider,  schemaFormDecoratorsProvider, sfBuilderProvider, sfPathProvider) {
 
-  var locStr = function(name, schema, options) {
-    if (schema.type === 'object' && schema.format == 'localizedString') {
-      var f = schemaFormProvider.stdFormObj(name, schema, options);
-      f.key  = options.path;
-      f.type = 'localizedstring';
-      if(!f.locales) {
-        f.locales = ['en'];
-      }
-      f.validationMessage = {
-        completed: 'All localized fields must be completed'
-      };
-      f.$validators = {
-        completed: function(value) {
-          if (value && Object.keys(value).length !== 0) {
-            var count = f.locales.map(function(locale) {
-              return value.hasOwnProperty(locale) ? 1 : 0;
-            }).reduce(function (previous, current) {
-              return previous + current;
-            });
-            return f.locales.length === count;
-          }
-          return true;
+    var locStr = function(name, schema, options) {
+      if (schema.type === 'object' && schema.format == 'localizedString') {
+        var f = schemaFormProvider.stdFormObj(name, schema, options);
+        f.key  = options.path;
+        f.type = 'localizedstring';
+        if(!f.locales) {
+          f.locales = ['en'];
         }
-      };
-      options.lookup[sfPathProvider.stringify(options.path)] = f;
-      return f;
-    }
-  };
-
-  schemaFormProvider.defaults.object.unshift(locStr);
-  
-  schemaFormDecoratorsProvider.defineAddOn(
-    'bootstrapDecorator',           // Name of the decorator you want to add to.
-    'localizedstring',                      // Form type that should render this add-on
-    'src/templates/sf-localized-string.html',  // Template name in $templateCache
-    sfBuilderProvider.stdBuilders   // List of builder functions to apply.
-  );
-
-}])
-    .controller('LocalizedStringController', ['$scope', function($scope){
-      $scope.$watch('ngModel.$modelValue', function() {
-        if ($scope.ngModel.$validate) {
-          $scope.ngModel.$validate();
-          if ($scope.ngModel.$invalid) { // The field must be made dirty so the error message is displayed
-            $scope.ngModel.$dirty = true;
-            $scope.ngModel.$pristine = false;
-          }
-        }
-        else {
-          $scope.ngModel.$setViewValue(ngModel.$viewValue);
+        f.validationMessage = {
+          completed: 'All localized fields must be completed'
         };
-      }, true);
+        f.$validators = {
+          completed: function(value) {
+            if (value && Object.keys(value).length > 0) {
+              return Object.keys(value).filter(function(key){
+                return f.locales.indexOf(key) > -1 && value[key] && '' !== value[key];
+              }).length === f.locales.length;
+            }
 
-      $scope.$watch('form', function() {
-        $scope.selectedLocale = $scope.form ? $scope.form.locales[0] : '';
-      });
+            return true;
+          }
+        };
+        options.lookup[sfPathProvider.stringify(options.path)] = f;
+        return f;
+      }
+    };
 
-      $scope.selectLocale = function (locale) {
-        $scope.selectedLocale = locale;
-        $scope.open = false;
-      };
+    schemaFormProvider.defaults.object.unshift(locStr);
 
-      $scope.toggleDropdown = function() {
-        $scope.open = !$scope.open;
-      };
+    schemaFormDecoratorsProvider.defineAddOn(
+      'bootstrapDecorator',           // Name of the decorator you want to add to.
+      'localizedstring',                      // Form type that should render this add-on
+      'src/templates/sf-localized-string.html',  // Template name in $templateCache
+      sfBuilderProvider.stdBuilders   // List of builder functions to apply.
+    );
 
+  }])
+  .controller('LocalizedStringController', ['$scope', function($scope){
+    $scope.$watch('ngModel.$modelValue', function() {
+      if ($scope.ngModel.$validate) {
+        // Make sure that allowInvalid is always true so that the model is preserved when validation fails
+        $scope.ngModel.$options = $scope.ngModel.$options || {};
+        $scope.ngModel.$options = {allowInvalid: true};
+        $scope.ngModel.$validate();
+        if ($scope.ngModel.$invalid) { // The field must be made dirty so the error message is displayed
+          $scope.ngModel.$dirty = true;
+          $scope.ngModel.$pristine = false;
+        }
+      }
+      else {
+        $scope.ngModel.$setViewValue(ngModel.$viewValue);
+      }
+    }, true);
+
+    $scope.$watch('form', function() {
+      $scope.form.disableErrorState = $scope.form.hasOwnProperty('readonly') && $scope.form.readonly;
+      $scope.selectedLocale = $scope.form.locales && $scope.form.locales.length > 0 ? $scope.form.locales[0] : '';
+    });
+
+    $scope.selectLocale = function (locale) {
+      $scope.selectedLocale = locale;
       $scope.open = false;
+    };
 
-    }]);
+    $scope.toggleDropdown = function() {
+      $scope.open = !$scope.open;
+    };
+
+    $scope.open = false;
+
+  }]);
