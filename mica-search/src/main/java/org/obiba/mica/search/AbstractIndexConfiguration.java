949a30b0e7c6150c848fb3a18eafb3e988cfb79b
Alex Prudencio <aprudencio@maelstrom-research.org>
2016-08-17 17:14:04 -0400

    MK-983 create elasticsearch indices from taxonomy

--- mica-search/src/main/java/org/obiba/mica/search/AbstractIndexConfiguration.java
+++ mica-search/src/main/java/org/obiba/mica/search/AbstractIndexConfiguration.java
@@ -11,22 +11,54 @@
 package org.obiba.mica.search;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import javax.inject.Inject;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.obiba.mica.core.domain.TaxonomyTarget;
 import org.obiba.mica.micaConfig.service.MicaConfigService;
+import org.obiba.mica.micaConfig.service.TaxonomyConfigService;
+import org.obiba.opal.core.domain.taxonomy.Taxonomy;
+import org.obiba.opal.core.domain.taxonomy.Vocabulary;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import sun.util.locale.LanguageTag;
 
-public class AbstractIndexConfiguration {
+public abstract class AbstractIndexConfiguration {
   private static final Logger log = LoggerFactory.getLogger(AbstractIndexConfiguration.class);
+  private static final String TRUE = "true";
+  private static final String LOCALIZED = "localized";
+  private static final String STATIC = "static";
+  private static final String FIELD = "field";
 
   @Inject
   private MicaConfigService micaConfigService;
+
+  @Inject
+  private TaxonomyConfigService taxonomyConfigService;
+
+  protected Taxonomy getTaxonomy() {
+    return taxonomyConfigService.findByTarget(getTarget());
+  }
+
+  protected void addLocalizedVocabularies(Taxonomy taxonomy, String... fields) {
+    Arrays.asList(fields).forEach(f -> {
+      taxonomy.addVocabulary(newVocabularyBuilder().name(f).field(f).localized().build());
+    });
+  }
+
+  protected void addStaticVocabularies(Taxonomy taxonomy, String... fields) {
+    Arrays.asList(fields).forEach(f -> {
+      taxonomy.addVocabulary(newVocabularyBuilder().name(f).field(f).staticField().build());
+    });
+  }
 
   protected void createLocalizedMappingWithAnalyzers(XContentBuilder mapping, String name) {
     try {
@@ -83,7 +115,6 @@ public class AbstractIndexConfiguration {
           .field("index", "not_analyzed")
         .endObject()
       .endObject();
-
   }
 
   protected void appendMembershipProperties(XContentBuilder mapping) throws IOException {
@@ -105,4 +136,148 @@ public class AbstractIndexConfiguration {
     membershipsMapping.endObject().endObject(); // memberships
   }
 
+  private void createSchemaMapping(XContentBuilder mapping, SchemaNode schema) throws IOException {
+    for(SchemaNode node : schema.getChildren()) {
+      if(node.getVocabulary() != null) {
+        Vocabulary v = node.getVocabulary();
+        if(TRUE.equals(v.getAttributeValue(LOCALIZED))) {
+          createLocalizedMappingWithAnalyzers(mapping, node.getName());
+        } else if(v.hasTerms() || TRUE.equals(v.getAttributeValue(STATIC))){
+          createMappingWithoutAnalyzer(mapping, node.getName());
+        }
+      } else {
+        mapping.startObject(node.getName()).startObject("properties");
+        createSchemaMapping(mapping, node);
+        mapping.endObject().endObject();
+      }
+    }
+  }
+
+  private void insertInSchema(SchemaNode schema, List<String> path, final Vocabulary vocabulary) {
+    final String head = path.get(0);
+
+    if(path.size() == 1) {
+      schema.addChild(new SchemaNode(head, vocabulary));
+      return;
+    }
+
+    insertInSchema(schema.getChild(head).orElseGet(() -> {
+        SchemaNode newChild = new SchemaNode(head);
+        schema.addChild(newChild);
+        return newChild;
+      }),
+      path.subList(1, path.size()), vocabulary);
+  }
+
+  protected void addTaxonomyFields(XContentBuilder mapping, Taxonomy taxonomy, List<String> ignore) throws IOException {
+    if(getTarget() != null) {
+      SchemaNode root = new SchemaNode();
+      taxonomy.getVocabularies().stream().forEach(v -> {
+        String fieldName = v.getAttributeValue(FIELD);
+        if(!ignore.contains(fieldName)) {
+          fieldName = fieldName != null ? fieldName : v.getName();
+          List<String> path = Lists.newArrayList(fieldName.split("\\."));
+
+          if(TRUE.equals(v.getAttributeValue(LOCALIZED)) && "und".equals(path.get(path.size()- 1))) {
+            path.remove(path.size() - 1);
+          }
+
+          insertInSchema(root, path, v);
+        }
+      });
+
+      createSchemaMapping(mapping, root);
+    }
+  }
+
+  protected TaxonomyTarget getTarget() {
+    return null;
+  }
+
+  protected VocabularyBuilder newVocabularyBuilder() {
+    return new VocabularyBuilder();
+  }
+
+  private class SchemaNode {
+    String name;
+    private List<SchemaNode> children = Lists.newArrayList();
+    private Vocabulary vocabulary;
+
+    public SchemaNode() {
+    }
+
+    public SchemaNode(String name) {
+      this.name = name;
+    }
+
+    public SchemaNode(String name, Vocabulary vocabulary) {
+      this.name = name;
+      this.vocabulary = vocabulary;
+    }
+
+    public String getName() {
+      return name;
+    }
+
+    public void setName(String name) {
+      this.name = name;
+    }
+
+    public Optional<SchemaNode> getChild(String name) {
+      return  children.stream().filter(c -> c.getName().equals(name)).findFirst();
+    }
+
+    public void addChild(SchemaNode node) {
+      children.add(node);
+    }
+
+    public List<SchemaNode> getChildren() {
+      return children;
+    }
+
+    public void setChildren(List<SchemaNode> children) {
+      this.children = children;
+    }
+
+    public Vocabulary getVocabulary() {
+      return vocabulary;
+    }
+
+    public void setVocabulary(Vocabulary vocabulary) {
+      this.vocabulary = vocabulary;
+    }
+  }
+
+
+  public class VocabularyBuilder {
+    String name;
+    Map<String, String> attributes = Maps.newHashMap();
+
+    public VocabularyBuilder name(String name) {
+      this.name = name;
+      return this;
+    }
+
+    public VocabularyBuilder field(String field) {
+      attributes.put(FIELD, field);
+      return this;
+    }
+
+    public VocabularyBuilder staticField() {
+      attributes.put(STATIC, TRUE);
+      return this;
+    }
+
+    public VocabularyBuilder localized() {
+      attributes.put(LOCALIZED, TRUE);
+      return this;
+    }
+
+    public Vocabulary build() {
+      Vocabulary v = new Vocabulary();
+      v.setName(name);
+      v.setAttributes(attributes);
+      return v;
+    }
+  }
 }
