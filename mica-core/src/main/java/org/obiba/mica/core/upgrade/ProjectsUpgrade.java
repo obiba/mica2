6c6d7b953895124dd5ea43a9d72079d160f52995
Cedric Fontin <cfontin@maelstrom-research.org>
2016-08-23 14:50:20 -0400

    MK-995 - dao's project permissions no longer in authorizingRealm but in db

--- mica-core/src/main/java/org/obiba/mica/core/upgrade/ProjectsUpgrade.java
+++ mica-core/src/main/java/org/obiba/mica/core/upgrade/ProjectsUpgrade.java
@@ -4,6 +4,8 @@ import javax.inject.Inject;
 
 import org.obiba.mica.access.event.DataAccessRequestUpdatedEvent;
 import org.obiba.mica.access.service.DataAccessRequestService;
+import org.obiba.mica.security.Roles;
+import org.obiba.mica.security.service.SubjectAclService;
 import org.obiba.runtime.Version;
 import org.obiba.runtime.upgrade.UpgradeStep;
 import org.slf4j.Logger;
@@ -18,6 +20,9 @@ public class ProjectsUpgrade implements UpgradeStep {
 
   @Inject
   private DataAccessRequestService dataAccessRequestService;
+
+  @Inject
+  private SubjectAclService subjectAclService;
 
   @Inject
   private EventBus eventBus;
@@ -36,5 +41,14 @@ public class ProjectsUpgrade implements UpgradeStep {
   public void execute(Version version) {
     log.info("Create a research project for each data access request.");
     dataAccessRequestService.findAll(null).forEach(dar -> eventBus.post(new DataAccessRequestUpdatedEvent(dar)));
+    addGroupPermissionsForDao();
+  }
+
+  private void addGroupPermissionsForDao() {
+    log.info("Adding group permissions on projects (draft and published) for mica-data-access-officer");
+    subjectAclService.addGroupPermission(Roles.MICA_DAO, "/draft/project", "*", null);
+    subjectAclService.addGroupPermission(Roles.MICA_DAO, "/draft/file", "*", "/project");
+    subjectAclService.addGroupPermission(Roles.MICA_DAO, "/project", "*", null);
+    subjectAclService.addGroupPermission(Roles.MICA_DAO, "/file", "*", "/project");
   }
 }
