04e960d9a67151318aa4f5ce31a941097e82d3b5
Cedric Fontin <cfontin@maelstrom-research.org>
2016-10-07 14:24:50 -0400

    MK-1086 - remove 'FormDirtyStateObserver' because it will be in ng-obiba-mica

--- mica-webapp/src/main/webapp/app/services.js
+++ mica-webapp/src/main/webapp/app/services.js
@@ -185,51 +185,6 @@ mica.service('AuthenticationSharedService', ['$rootScope', '$q', '$http', '$cook
       };
   }]);
 
-mica.factory('FormDirtyStateObserver', ['$uibModal', '$location',
-  function ($uibModal, $location) {
-    var onLocationChangeOff;
-
-    return {
-      observe: function(scope) {
-        if (onLocationChangeOff) {
-          onLocationChangeOff();
-        }
-
-        onLocationChangeOff = scope.$on('$locationChangeStart', function (event, newUrl) {
-          if (scope.form.$dirty) {
-            $uibModal.open({
-              backdrop: 'static',
-              controller: ['$scope', '$uibModalInstance', function ($scope, $uibModalInstance) {
-                $scope.ok = function () {
-                  $uibModalInstance.close(true);
-                };
-                $scope.cancel = function () {
-                  $uibModalInstance.dismiss('cancel');
-                };
-              }],
-              templateUrl: 'app/views/unsaved-modal.html'
-            }).result.then(function (answer) {
-              if (answer === true) {
-                onLocationChangeOff();
-                $location.path($location.url(newUrl).hash());
-              }
-            });
-
-            event.preventDefault();
-            return;
-          }
-
-          onLocationChangeOff();
-        });
-      },
-      unobserve: function() {
-        if(onLocationChangeOff) {
-          onLocationChangeOff();
-        }
-      }
-    };
-  }]);
-
 mica.factory('MetricsService', ['$resource',
   function ($resource) {
     return $resource('jvm', {}, {
