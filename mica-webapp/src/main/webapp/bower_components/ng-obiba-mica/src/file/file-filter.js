df4ee915b6bc48262757b9ec0d8074e17c8cc090
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2016-10-12 14:25:35 -0400

    MK-1092 - removec node and bower dependencies from code base.

--- mica-webapp/src/main/webapp/bower_components/ng-obiba-mica/src/file/file-filter.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('obiba.mica.file')
-  .filter('bytes', function () {
-    return function (bytes) {
-      return bytes === null || typeof bytes === 'undefined' ? '' : filesize(bytes);
-    };
-  });
-
