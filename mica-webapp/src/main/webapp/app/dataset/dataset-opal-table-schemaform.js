da3ead58c10ee81dcf4a62f972af9d596ea91083
Yannick Marcon <yannick.marcon@obiba.org>
2016-11-10 12:55:48 +0100

    MK-1032 Opal tables form uses SF in the modal

---
+++ mica-webapp/src/main/webapp/app/dataset/dataset-opal-table-schemaform.js
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2016 OBiBa. All rights reserved.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* exported OPAL_TABLE_SCHEMA*/
+var OPAL_TABLE_SCHEMA = {
+  type: 'object',
+  properties: {
+    name: {
+      title: 't(dataset.table-name)',
+      type: 'object',
+      format: 'localizedString'
+    },
+    description: {
+      title: 't(dataset.table-description)',
+      type: 'object',
+      format: 'localizedString'
+    }
+  }
+};
+
+/* exported OPAL_TABLE_DEFINITION */
+var OPAL_TABLE_DEFINITION = [{
+  type: 'fieldset',
+  items: [
+    {
+      key: 'name',
+      type: 'localizedstring'
+    },
+    {
+      key: 'description',
+      type: 'localizedstring',
+      rows: 5
+    }
+  ]
+}];
