e0c02e114e7bc1dd911c4d921382761852d94f9b
Romain Roces <romain.roces+github@gmail.com>
2016-11-28 17:19:41 -0500

    Migrate mica from 1.5 to 2.0

--- mica-core/src/main/java/org/obiba/mica/network/NetworkRepository.java
+++ mica-core/src/main/java/org/obiba/mica/network/NetworkRepository.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 import org.obiba.mica.network.domain.Network;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 /**
  * Spring Data MongoDB repository for the {@link org.obiba.mica.network.domain.Network} entity.
@@ -24,4 +25,6 @@ public interface NetworkRepository extends MongoRepository<Network, String>, Net
 
   List<Network> findByNetworkIds(String networkId);
 
+  @Query("{'model' : { $exists : false }}")
+  List<Network> findWithoutModel();
 }
--- mica-core/src/main/java/org/obiba/mica/study/StudyRepository.java
+++ mica-core/src/main/java/org/obiba/mica/study/StudyRepository.java
@@ -12,10 +12,16 @@ package org.obiba.mica.study;
 
 import org.obiba.mica.study.domain.Study;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 /**
  * Spring Data MongoDB repository for the Study entity.
  */
 
 public interface StudyRepository extends MongoRepository<Study, String>, StudyRepositoryCustom {
+
+  @Query("{'model' : { $exists : false }}")
+  List<Study> findWithoutModel();
 }
