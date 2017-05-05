13efdf37769f1541349305b75f242bd333fb1baa
Romain Roces <romain.roces@gmail.com>
2017-05-05 17:00:30 -0400

    MK-1182 Rollback unwanted code

--- mica-webapp/src/main/java/org/obiba/mica/Application.java
+++ mica-webapp/src/main/java/org/obiba/mica/Application.java
@@ -57,7 +57,6 @@ public class Application {
    */
   public static void main(String... args) throws InterruptedException {
 
-    System.setProperty("MICA_HOME", "/home/rroces/DevSrc/obiba/mica2-home/");
     checkSystemProperty("MICA_HOME");
 
     SpringApplication app = new SpringApplication(Application.class);
