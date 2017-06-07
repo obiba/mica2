65552714cbfb2bcaea146ba44ef9ed573570d65d
Cedric Fontin <cfontin@maelstrom-research.org>
2017-06-07 10:56:18 -0400

    MK-1361 - update step that republishes studies with no revisions ahead

---
+++ mica-core/src/main/java/org/obiba/mica/core/upgrade/Mica3Upgrade.java
@@ -0,0 +1,51 @@
+package org.obiba.mica.core.upgrade;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.inject.Inject;
+
+import org.obiba.mica.core.domain.AbstractGitPersistable;
+import org.obiba.mica.study.service.StudyService;
+import org.obiba.runtime.Version;
+import org.obiba.runtime.upgrade.UpgradeStep;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Mica3Upgrade implements UpgradeStep {
+
+  private static final Logger logger = LoggerFactory.getLogger(Mica3Upgrade.class);
+
+  @Inject
+  private StudyService studyService;
+
+  @Override
+  public String getDescription() {
+    return "Mica 3.0.0 upgrade";
+  }
+
+  @Override
+  public Version getAppliesTo() {
+    return new Version(3, 0, 0);
+  }
+
+  @Override
+  public void execute(Version version) {
+    logger.info("Executing Mica upgrade to version 3.0.0");
+
+    try {
+      republishStudies();
+    } catch (Exception e) {
+      logger.error("Error occurred when republishing studies");
+    }
+  }
+
+  private void republishStudies() {
+    studyService.findPublishedStates().stream()
+      .filter(s -> s.getRevisionsAhead() == 0)
+      .map(AbstractGitPersistable::getId)
+      .forEach(s -> studyService.publish(s, true));
+  }
+}
