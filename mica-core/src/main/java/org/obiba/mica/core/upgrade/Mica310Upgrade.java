ce86972783a59ff44802b88a40d0817541969a8f
Romain Roces <romain.roces@gmail.com>
2017-10-10 17:05:02 -0400

    MK-1568 Set correct facet to show in study list page

--- mica-core/src/main/java/org/obiba/mica/core/upgrade/Mica310Upgrade.java
+++ mica-core/src/main/java/org/obiba/mica/core/upgrade/Mica310Upgrade.java
@@ -76,8 +76,25 @@ public class Mica310Upgrade implements UpgradeStep {
 
     logger.info("Add default facets in study taxonomy");
     ImmutableMap<String, String> vocabularyWithPriority = ImmutableMap.<String, String>builder()
-      .put("objectives", "3")
-      .put("populations-dataCollectionEvents-dataSources", "10")
+      .put("objectives", "1")
+      .put("methods-design", "2")
+      .put("investigator", "3")
+      .put("start", "4")
+      .put("end", "5")
+      .put("populations-selectionCriteria-countriesIso", "6")
+      .put("populations-selectionCriteria-ageMin", "7")
+      .put("populations-selectionCriteria-ageMax", "8")
+      .put("populations-selectionCriteria-gender", "9")
+      .put("populations-selectionCriteria-pregnantWomen", "10")
+      .put("populations-selectionCriteria-newborn", "11")
+      .put("populations-selectionCriteria-twins", "12")
+      .put("numberOfParticipants-participant-range", "13")
+      .put("numberOfParticipants-sample-range", "14")
+      .put("methods-recruitments", "15")
+      .put("populations-recruitment-dataSources", "16")
+      .put("populations-dataCollectionEvents-dataSources", "17")
+      .put("populations-dataCollectionEvents-bioSamples", "18")
+      .put("access", "19")
       .build();
 
     Taxonomy studyTaxonomy = taxonomyConfigService.findByTarget(TaxonomyTarget.STUDY);
