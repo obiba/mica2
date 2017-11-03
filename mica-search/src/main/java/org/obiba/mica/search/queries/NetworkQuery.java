0ee39f58a109c7665b33d695cbc478074cbf1dee
Romain Roces <romain.roces@gmail.com>
2017-11-03 14:12:33 -0400

    MK-1641 Fix search dataset count details in network tab

--- mica-search/src/main/java/org/obiba/mica/search/queries/NetworkQuery.java
+++ mica-search/src/main/java/org/obiba/mica/search/queries/NetworkQuery.java
@@ -18,7 +18,11 @@ import org.obiba.mica.network.domain.NetworkState;
 import org.obiba.mica.network.service.PublishedNetworkService;
 import org.obiba.mica.search.aggregations.NetworkAggregationMetaDataProvider;
 import org.obiba.mica.search.aggregations.NetworkTaxonomyMetaDataProvider;
-import org.obiba.mica.spi.search.*;
+import org.obiba.mica.spi.search.CountStatsData;
+import org.obiba.mica.spi.search.Indexer;
+import org.obiba.mica.spi.search.QueryMode;
+import org.obiba.mica.spi.search.QueryScope;
+import org.obiba.mica.spi.search.Searcher;
 import org.obiba.mica.web.model.Dtos;
 import org.obiba.mica.web.model.Mica;
 import org.obiba.mica.web.model.MicaSearch;
@@ -30,7 +34,11 @@ import org.springframework.stereotype.Component;
 import javax.annotation.Nullable;
 import javax.inject.Inject;
 import java.io.IOException;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
@@ -113,7 +121,11 @@ public class NetworkQuery extends AbstractDocumentQuery {
   private Consumer<Network> getNetworkConsumer(QueryScope scope, NetworkResultDto.Builder resBuilder,
                                                NetworkCountStatsBuilder networkCountStatsBuilder) {
 
-    return scope == QueryScope.DETAIL ? (network) -> {
+    return scope == QueryScope.DETAIL ? getNetworkConsumer(resBuilder, networkCountStatsBuilder) : (network) -> resBuilder.addDigests(dtos.asDigestDtoBuilder(network).build());
+  }
+
+  private Consumer<Network> getNetworkConsumer(NetworkResultDto.Builder resBuilder, NetworkCountStatsBuilder networkCountStatsBuilder) {
+    return (network) -> {
       Mica.NetworkDto.Builder networkBuilder = dtos.asDtoBuilder(network);
       if (mode == QueryMode.LIST) {
         networkBuilder.clearStudySummaries();
@@ -125,7 +137,7 @@ public class NetworkQuery extends AbstractDocumentQuery {
             .build();
       }
       resBuilder.addNetworks(networkBuilder.build());
-    } : (network) -> resBuilder.addDigests(dtos.asDigestDtoBuilder(network).build());
+    };
   }
 
   @Override
