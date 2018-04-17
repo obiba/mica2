eeaf6f95bc22a279c1df0eb2bdc9d1691a47c6b2
Yannick Marcon <yannick.marcon@obiba.org>
2018-04-17 09:55:59 +0200

    MK-1819 Search python command is able to extract models and annotations

--- mica-python-client/src/main/python/mica/search.py
+++ mica-python-client/src/main/python/mica/search.py
@@ -65,15 +65,17 @@ def append_rql(query, target, select, sort, start, limit, locale):
 
     return q + ',locale(' + locale + ')'
 
-def extract_label(labels, locale='en', localeKey='lang', valueKey='value'):
+def extract_label(labels, locale='en', locale_key='lang', value_key='value'):
+    if not labels:
+        return None
     encoder = codecs.getincrementalencoder('utf-8')()
     label_und = None
     if labels:
         for label in labels:
-            if label[localeKey] == locale:
-                return encoder.encode(label[valueKey])
-            if label[localeKey] == 'und':
-                label_und = label[valueKey]
+            if label[locale_key] == locale:
+                return encoder.encode(label[value_key])
+            if label[locale_key] == 'und':
+                label_und = label[value_key]
     return encoder.encode(label_und)
 
 def new_writer(out, headers):
@@ -84,75 +86,153 @@ def new_writer(out, headers):
     writer.writeheader()
     return writer
 
+def to_string(value):
+    if value == None:
+        return ''
+    return str(value)
+
+def flatten(content):
+    flat = {}
+    for key in content.keys():
+        value = content[key]
+        if type(value) is dict:
+            fvalue = flatten(value)
+            for k in fvalue:
+                flat[key + '.' + k] = fvalue[k]
+        elif type(value) is list:
+            encoder = codecs.getincrementalencoder('utf-8')()
+            flat[key] = encoder.encode('|'.join(map(to_string, value)))
+        else:
+            encoder = codecs.getincrementalencoder('utf-8')()
+            flat[key] = encoder.encode(value) if type(value) is unicode else value
+    return flat
+
 def search_networks(args, client):
-    q = append_rql(args.query, 'network', ['acronym', 'name', 'studyIds'], ['id'], args.start, args.limit, args.locale)
+    q = append_rql(args.query, 'network', ['acronym', 'name', 'studyIds','model'], ['id'], args.start, args.limit, args.locale)
     ws = mica.core.UriBuilder(['networks', '_rql']).build()
     res = send_search_request(client, ws, q, args.verbose)
-    writer = new_writer(args.out, ['id','name','acronym','studyIds'])
     if 'networkResultDto' in res and 'obiba.mica.NetworkResultDto.result' in res['networkResultDto']:
+        headers = ['id','name','acronym','studyIds']
         for item in res['networkResultDto']['obiba.mica.NetworkResultDto.result']['networks']:
-            writer.writerow({
+            if 'content' in item:
+                item['flat'] = flatten(json.loads(item['content']))
+                for key in item['flat'].keys():
+                    if key not in headers:
+                        headers.append(key)
+        writer = new_writer(args.out, headers)
+        for item in res['networkResultDto']['obiba.mica.NetworkResultDto.result']['networks']:
+            row = {
                 'id': item['id'],
                 'name': extract_label(item['name'], args.locale),
                 'acronym': extract_label(item['acronym'], args.locale),
                 'studyIds': '|'.join(item['studyIds'])
-            })
+            }
+            if 'flat' in item:
+                for key in item['flat']:
+                    row[key] = item['flat'][key]
+            writer.writerow(row)
 
 def search_studies(args, client):
-    q = append_rql(args.query, 'study', ['acronym', 'name'], ['id'], args.start, args.limit, args.locale)
+    q = append_rql(args.query, 'study', ['acronym', 'name', 'model'], ['id'], args.start, args.limit, args.locale)
     ws = mica.core.UriBuilder(['studies', '_rql']).build()
     res = send_search_request(client, ws, q, args.verbose)
-    writer = new_writer(args.out, ['id','name','acronym'])
     if 'studyResultDto' in res and 'obiba.mica.StudyResultDto.result' in res['studyResultDto']:
+        headers = ['id','name','acronym']
         for item in res['studyResultDto']['obiba.mica.StudyResultDto.result']['summaries']:
-            writer.writerow({
+            if 'content' in item:
+                item['flat'] = flatten(json.loads(item['content']))
+                for key in item['flat'].keys():
+                    if key not in headers:
+                        headers.append(key)
+        writer = new_writer(args.out, headers)
+        for item in res['studyResultDto']['obiba.mica.StudyResultDto.result']['summaries']:
+            row = {
                 'id': item['id'],
                 'name': extract_label(item['name'], args.locale),
                 'acronym': extract_label(item['acronym'], args.locale)
-            })
+            }
+            if 'flat' in item:
+                for key in item['flat']:
+                    row[key] = item['flat'][key]
+            writer.writerow(row)
 
 def search_study_populations(args, client):
-    q = append_rql(args.query, 'study', ['populations.name'], ['id'], args.start, args.limit, args.locale)
+    q = append_rql(args.query, 'study', ['populations.name','populations.model'], ['id'], args.start, args.limit, args.locale)
     ws = mica.core.UriBuilder(['studies', '_rql']).build()
     res = send_search_request(client, ws, q, args.verbose)
-    writer = new_writer(args.out, ['id','name','studyId'])
     if 'studyResultDto' in res and 'obiba.mica.StudyResultDto.result' in res['studyResultDto']:
+        headers = ['id','name','studyId']
         for item in res['studyResultDto']['obiba.mica.StudyResultDto.result']['summaries']:
             if 'populationSummaries' in item:
                 for pop in item['populationSummaries']:
-                    writer.writerow({
+                    if 'content' in pop:
+                        pop['flat'] = flatten(json.loads(pop['content']))
+                        for key in pop['flat'].keys():
+                            if key not in headers:
+                                headers.append(key)
+        writer = new_writer(args.out, headers)
+        for item in res['studyResultDto']['obiba.mica.StudyResultDto.result']['summaries']:
+            if 'populationSummaries' in item:
+                for pop in item['populationSummaries']:
+                    row = {
                         'id': item['id'] + ':' + pop['id'],
                         'name': extract_label(pop['name'], args.locale),
                         'studyId': item['id']
-                    })
+                    }
+                    if 'flat' in pop:
+                        for key in pop['flat']:
+                            row[key] = pop['flat'][key]
+                    writer.writerow(row)
 
 def search_study_dces(args, client):
     q = append_rql(args.query, 'study', ['populations.dataCollectionEvents'], ['id'], args.start, args.limit, args.locale)
     ws = mica.core.UriBuilder(['studies', '_rql']).build()
     res = send_search_request(client, ws, q, args.verbose)
-    writer = new_writer(args.out, ['id','name','studyId', 'populationId', 'start', 'end'])
     if 'studyResultDto' in res and 'obiba.mica.StudyResultDto.result' in res['studyResultDto']:
+        headers = ['id','name','studyId', 'populationId', 'start', 'end']
         for item in res['studyResultDto']['obiba.mica.StudyResultDto.result']['summaries']:
             if 'populationSummaries' in item:
                 for pop in item['populationSummaries']:
                     if 'dataCollectionEventSummaries' in pop:
                         for dce in pop['dataCollectionEventSummaries']:
-                            writer.writerow({
+                            if 'content' in dce:
+                                dce['flat'] = flatten(json.loads(dce['content']))
+                                for key in dce['flat'].keys():
+                                    if key not in headers:
+                                        headers.append(key)
+        writer = new_writer(args.out, headers)
+        for item in res['studyResultDto']['obiba.mica.StudyResultDto.result']['summaries']:
+            if 'populationSummaries' in item:
+                for pop in item['populationSummaries']:
+                    if 'dataCollectionEventSummaries' in pop:
+                        for dce in pop['dataCollectionEventSummaries']:
+                            row = {
                                 'id': item['id'] + ':' + pop['id'] + dce['id'],
                                 'name': extract_label(dce['name'], args.locale),
                                 'studyId': item['id'],
                                 'populationId': item['id'] + ':' + pop['id'],
                                 'start': dce['start'] if 'start' in dce else '',
                                 'end': dce['end'] if 'end' in dce else ''
-                            })
+                            }
+                            if 'flat' in dce:
+                                for key in dce['flat']:
+                                    row[key] = dce['flat'][key]
+                            writer.writerow(row)
 
 
 def search_datasets(args, client):
-    q = append_rql(args.query, 'dataset', ['acronym', 'name', 'studyTable', 'harmonizationTable'], ['id'], args.start, args.limit, args.locale)
+    q = append_rql(args.query, 'dataset', ['acronym', 'name', 'studyTable', 'harmonizationTable','model'], ['id'], args.start, args.limit, args.locale)
     ws = mica.core.UriBuilder(['datasets', '_rql']).build()
     res = send_search_request(client, ws, q, args.verbose)
-    writer = new_writer(args.out, ['id','name','acronym', 'variableType', 'entityType', 'studyId', 'populationId', 'dceId'])
     if 'datasetResultDto' in res and 'obiba.mica.DatasetResultDto.result' in res['datasetResultDto']:
+        headers = ['id','name','acronym', 'variableType', 'entityType', 'studyId', 'populationId', 'dceId']
+        for item in res['datasetResultDto']['obiba.mica.DatasetResultDto.result']['datasets']:
+            if 'content' in item:
+                item['flat'] = flatten(json.loads(item['content']))
+                for key in item['flat'].keys():
+                    if key not in headers:
+                        headers.append(key)
+        writer = new_writer(args.out, headers)
         for item in res['datasetResultDto']['obiba.mica.DatasetResultDto.result']['datasets']:
             study_id = ''
             population_id = ''
@@ -164,7 +244,7 @@ def search_datasets(args, client):
             if 'obiba.mica.HarmonizedDatasetDto.type' in item:
                 study_id = item['obiba.mica.HarmonizedDatasetDto.type']['harmonizationTable']['studyId']
                 population_id = study_id + ':' + item['obiba.mica.HarmonizedDatasetDto.type']['harmonizationTable']['populationId']
-            writer.writerow({
+            row = {
                 'id': item['id'],
                 'name': extract_label(item['name'], args.locale),
                 'acronym': extract_label(item['acronym'], args.locale),
@@ -173,16 +253,27 @@ def search_datasets(args, client):
                 'studyId': study_id,
                 'populationId': population_id,
                 'dceId': dce_id
-            })
+            }
+            if 'flat' in item:
+                for key in item['flat']:
+                    row[key] = item['flat'][key]
+            writer.writerow(row)
 
 def search_variables(args, client):
     q = append_rql(args.query, 'variable', ['attributes','nature','valueType'], ['id'], args.start, args.limit, args.locale)
     ws = mica.core.UriBuilder(['variables', '_rql']).build()
     res = send_search_request(client, ws, q, args.verbose)
-    writer = new_writer(args.out, ['id','name','label','valueType','nature','datasetId','studyId','variableType'])
     if 'variableResultDto' in res and 'obiba.mica.DatasetVariableResultDto.result' in res['variableResultDto']:
+        headers = ['id','name','label','valueType','nature','datasetId','studyId','variableType']
         for item in res['variableResultDto']['obiba.mica.DatasetVariableResultDto.result']['summaries']:
-            writer.writerow({
+            if 'annotations' in item:
+                for annot in item['annotations']:
+                    key = annot['taxonomy'] + '.' + annot['vocabulary']
+                    if key not in headers:
+                        headers.append(key)
+        writer = new_writer(args.out, headers)
+        for item in res['variableResultDto']['obiba.mica.DatasetVariableResultDto.result']['summaries']:
+            row = {
                 'id': item['id'],
                 'name': item['name'],
                 'label': extract_label(item['variableLabel'], args.locale),
@@ -191,7 +282,12 @@ def search_variables(args, client):
                 'variableType': item['variableType'],
                 'valueType': item['valueType'] if 'valueType' in item else '',
                 'nature': item['nature'] if 'nature' in item else ''
-            })
+            }
+            if 'annotations' in item:
+                for annot in item['annotations']:
+                    key = annot['taxonomy'] + '.' + annot['vocabulary']
+                    row[key] = annot['value']
+            writer.writerow(row)
 
 def do_command(args):
     '''
