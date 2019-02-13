3697578a80c6f787fd58e1e9a2dd6ae256d5bad3
meek0 <cedricgfont@gmail.com>
2019-02-13 11:33:37 -0500

    send notification to those who can see private comments when one is added

--- mica-core/src/main/java/org/obiba/mica/access/notification/DataAccessRequestCommentMailNotification.java
+++ mica-core/src/main/java/org/obiba/mica/access/notification/DataAccessRequestCommentMailNotification.java
@@ -10,6 +10,10 @@
 
 package org.obiba.mica.access.notification;
 
+import static java.util.stream.Collectors.toList;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import javax.inject.Inject;
@@ -24,6 +28,9 @@ import org.obiba.mica.micaConfig.domain.DataAccessForm;
 import org.obiba.mica.micaConfig.service.DataAccessFormService;
 import org.obiba.mica.micaConfig.service.MicaConfigService;
 import org.obiba.mica.security.Roles;
+import org.obiba.mica.security.domain.SubjectAcl;
+import org.obiba.mica.security.domain.SubjectAcl.Type;
+import org.obiba.mica.security.service.SubjectAclService;
 import org.springframework.stereotype.Component;
 
 import com.google.common.base.Strings;
@@ -32,20 +39,33 @@ import com.google.common.collect.Maps;
 @Component
 public class DataAccessRequestCommentMailNotification implements MailNotification<Comment>  {
 
-  @Inject
   private DataAccessFormService dataAccessFormService;
 
-  @Inject
-  DataAccessRequestUtilService dataAccessRequestUtilService;
+  private DataAccessRequestUtilService dataAccessRequestUtilService;
 
-  @Inject
-  DataAccessRequestService dataAccessRequestService;
+  private DataAccessRequestService dataAccessRequestService;
 
-  @Inject
-  MailService mailService;
+  private MailService mailService;
 
-  @Inject
+  private SubjectAclService subjectAclService;
+
   private MicaConfigService micaConfigService;
+
+  @Inject
+  public DataAccessRequestCommentMailNotification(
+    DataAccessFormService dataAccessFormService,
+    DataAccessRequestUtilService dataAccessRequestUtilService,
+    DataAccessRequestService dataAccessRequestService,
+    MailService mailService,
+    SubjectAclService subjectAclService,
+    MicaConfigService micaConfigService) {
+    this.dataAccessFormService = dataAccessFormService;
+    this.dataAccessRequestUtilService = dataAccessRequestUtilService;
+    this.dataAccessRequestService = dataAccessRequestService;
+    this.mailService = mailService;
+    this.subjectAclService = subjectAclService;
+    this.micaConfigService = micaConfigService;
+  }
 
   @Override
   public void send(Comment comment) {
@@ -67,11 +87,31 @@ public class DataAccessRequestCommentMailNotification implements MailNotificatio
     ctx.put("applicant", request.getApplicant());
     ctx.put("status", request.getStatus().name());
 
-    mailService.sendEmailToUsers(mailService.getSubject(dataAccessForm.getCommentedSubject(), ctx, DataAccessRequestUtilService.DEFAULT_NOTIFICATION_SUBJECT),
-      "dataAccessRequestCommentAdded", ctx, request.getApplicant());
+    if (comment.getAdmin()) {
+      List<SubjectAcl> privateCommentsAcls = getPrivateCommentsAcls();
+
+      mailService.sendEmailToGroupsAndUsers(
+        dataAccessForm.getCommentedSubject(),
+        "dataAccessRequestCommentAdded",
+        ctx,
+        getAclForType(privateCommentsAcls, Type.GROUP),
+        getAclForType(privateCommentsAcls, Type.USER));
+
+    } else {
+      mailService.sendEmailToUsers(mailService.getSubject(dataAccessForm.getCommentedSubject(), ctx, DataAccessRequestUtilService.DEFAULT_NOTIFICATION_SUBJECT),
+        "dataAccessRequestCommentAdded", ctx, request.getApplicant());
+    }
 
     mailService.sendEmailToGroups(mailService.getSubject(dataAccessForm.getCommentedSubject(), ctx, DataAccessRequestUtilService.DEFAULT_NOTIFICATION_SUBJECT),
       "dataAccessRequestCommentAdded", ctx, Roles.MICA_DAO);
+  }
+
+  private List<SubjectAcl> getPrivateCommentsAcls() {
+    return subjectAclService.findByResourceInstance("/data-access-request/private-comment", "*");
+  }
+
+  private List<String> getAclForType(List<SubjectAcl> acls, Type type) {
+    return acls.stream().filter(acl -> type.equals(acl.getType())).map(SubjectAcl::getPrincipal).collect(toList());
   }
 
 }
