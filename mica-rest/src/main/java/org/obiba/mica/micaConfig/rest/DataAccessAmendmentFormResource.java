8183c41a0d8b46d473655d873022e97c6fe27f3e
Ramin Haeri Azad <rhaeri@maelstrom-reseach.org>
2019-02-18 16:04:04 -0500

    Added Conditionally Approved and Under Review as AND form config.

--- mica-rest/src/main/java/org/obiba/mica/micaConfig/rest/DataAccessAmendmentFormResource.java
+++ mica-rest/src/main/java/org/obiba/mica/micaConfig/rest/DataAccessAmendmentFormResource.java
@@ -11,7 +11,9 @@ import javax.ws.rs.core.Response;
 import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.obiba.mica.micaConfig.NoSuchDataAccessFormException;
 import org.obiba.mica.micaConfig.domain.DataAccessAmendmentForm;
+import org.obiba.mica.micaConfig.domain.DataAccessForm;
 import org.obiba.mica.micaConfig.service.DataAccessAmendmentFormService;
+import org.obiba.mica.micaConfig.service.DataAccessFormService;
 import org.obiba.mica.security.Roles;
 import org.obiba.mica.web.model.Dtos;
 import org.obiba.mica.web.model.Mica;
@@ -21,21 +23,27 @@ import org.springframework.stereotype.Component;
 @Path("/config/data-access-amendment-form")
 public class DataAccessAmendmentFormResource {
 
+  private DataAccessFormService dataAccessFormService;
   private DataAccessAmendmentFormService dataAccessAmendmentFormService;
 
   private Dtos dtos;
 
   @Inject
-  public DataAccessAmendmentFormResource(DataAccessAmendmentFormService dataAccessAmendmentFormService, Dtos dtos) {
+  public DataAccessAmendmentFormResource(DataAccessAmendmentFormService dataAccessAmendmentFormService,
+                                         DataAccessFormService dataAccessFormService,
+                                         Dtos dtos) {
     this.dataAccessAmendmentFormService = dataAccessAmendmentFormService;
+    this.dataAccessFormService = dataAccessFormService;
     this.dtos = dtos;
   }
 
   @GET
   public Mica.DataAccessAmendmentFormDto get() {
     Optional<DataAccessAmendmentForm> dataAccessAmendmentForm = dataAccessAmendmentFormService.find();
+    Optional<DataAccessForm> dataAccessForm = dataAccessFormService.find();
+    if(!dataAccessForm.isPresent()) throw NoSuchDataAccessFormException.withDefaultMessage();
     if (!dataAccessAmendmentForm.isPresent()) throw NoSuchDataAccessFormException.withMessage("DataAccessAmendmentForm does not exist");
-    return dtos.asDto(dataAccessAmendmentForm.get());
+    return dtos.asDto(dataAccessAmendmentForm.get(), dataAccessForm.get());
   }
 
   @PUT
