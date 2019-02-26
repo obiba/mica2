4f034ce19bd113638fba55f9a00048d66756aa49
meek0 <cedricgfont@gmail.com>
2019-02-26 14:55:47 -0500

    Config to give DAO edition rights to DAR's

--- mica-core/src/main/java/org/obiba/mica/security/service/SubjectAclService.java
+++ mica-core/src/main/java/org/obiba/mica/security/service/SubjectAclService.java
@@ -50,7 +50,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
