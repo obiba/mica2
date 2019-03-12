6ee6695d7af8bf397dee0c3918718d3f39b215f4
meek0 <cedricgfont@gmail.com>
2019-03-12 10:03:01 -0400

    ensure status change history profiles

--- mica-core/src/main/java/org/obiba/mica/web/model/StatusChangeDtos.java
+++ mica-core/src/main/java/org/obiba/mica/web/model/StatusChangeDtos.java
@@ -10,12 +10,14 @@
 
 package org.obiba.mica.web.model;
 
+import java.util.Map;
 import org.joda.time.DateTime;
 import org.obiba.mica.access.domain.DataAccessEntityStatus;
 import org.obiba.mica.access.domain.StatusChange;
 import org.obiba.mica.user.UserProfileService;
 import org.obiba.mica.web.model.Mica.DataAccessRequestDto.StatusChangeDto;
 import org.obiba.shiro.realm.ObibaRealm;
+import org.obiba.shiro.realm.ObibaRealm.Subject;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
