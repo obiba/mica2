5400db687416d570a560a58c54ffc2e51845fdea
Yannick Marcon <yannick.marcon@obiba.org>
2020-01-26 12:56:50 +0100

    #3833 jquery used to make deferred queries

--- mica-webapp/src/main/java/org/obiba/mica/web/controller/ErrorController.java
+++ mica-webapp/src/main/java/org/obiba/mica/web/controller/ErrorController.java
@@ -2,7 +2,13 @@ package org.obiba.mica.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.ws.rs.QueryParam;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class ErrorController {
@@ -10,6 +16,14 @@ public class ErrorController {
   @GetMapping("/error")
   public ModelAndView error() {
     return new ModelAndView("error");
+  }
+
+  @PostMapping(value = "/error", consumes = "application/x-www-form-urlencoded")
+  public ModelAndView errorWithParams(@RequestParam(defaultValue = "500") int status, @RequestParam(defaultValue = "") String message) {
+    Map<String, Object> params = new HashMap<String, Object>();
+    params.put("status", status);
+    params.put("message", message);
+    return new ModelAndView("error", params);
   }
 
 }
