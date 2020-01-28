9f84963946436ecc6be282a472782101c58046f8
Yannick Marcon <yannick.marcon@obiba.org>
2020-01-28 15:17:26 +0100

    #3833 Sign in and out added

--- mica-webapp/src/main/java/org/obiba/mica/web/controller/DataAccessesController.java
+++ mica-webapp/src/main/java/org/obiba/mica/web/controller/DataAccessesController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class DashboardController {
+public class DataAccessesController {
 
-  @GetMapping("/dashboard")
+  @GetMapping("/data-accesses")
   public ModelAndView get() {
-    return new ModelAndView("dashboard");
+    return new ModelAndView("data-accesses");
   }
 
 }
