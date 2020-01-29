a859f5b7f7a22b7e81d5f76aa2a72be7e47a8a65
Yannick Marcon <yannick.marcon@obiba.org>
2020-01-29 21:59:35 +0100

    ng search in progress

--- mica-webapp/src/main/java/org/obiba/mica/web/controller/CatalogController.java
+++ mica-webapp/src/main/java/org/obiba/mica/web/controller/CatalogController.java
@@ -4,18 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Controller
 public class CatalogController {
 
   @GetMapping("/catalog")
   public ModelAndView get() {
-    Map<String, Object> params = new HashMap<String, Object>();
-    params.put("message", "coucou");
-
-    return new ModelAndView("catalog", params);
+    return new ModelAndView("catalog");
   }
 
 }
