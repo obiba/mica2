9802eda741d4a1e2cb2345efaacc5f7ee59eb683
Ramin Haeri Azad <rhaeri@maelstrom-reseach.org>
2020-01-30 17:37:20 -0500

    Made sure investigator/contact fullname have proper mappings + added upgrade step.

--- mica-core/src/main/java/org/obiba/mica/config/UpgradeConfiguration.java
+++ mica-core/src/main/java/org/obiba/mica/config/UpgradeConfiguration.java
@@ -67,7 +67,8 @@ public class UpgradeConfiguration {
       applicationContext.getBean(Mica320Upgrade.class),
       applicationContext.getBean(Mica330Upgrade.class),
       applicationContext.getBean(Mica350Upgrade.class),
-      applicationContext.getBean(Mica372Upgrade.class)
+      applicationContext.getBean(Mica372Upgrade.class),
+      applicationContext.getBean(Mica380Upgrade.class)
     );
   }
 }
