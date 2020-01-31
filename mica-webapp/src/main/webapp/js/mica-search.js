b319341d8a9261d1f2d03b468b89ffe955797f2b
Yannick Marcon <yannick.marcon@obiba.org>
2020-01-31 16:08:06 +0100

    #3833 Using vuejs in search page

--- mica-webapp/src/main/webapp/js/mica-search.js
+++ mica-webapp/src/main/webapp/js/mica-search.js
@@ -1,306 +1,80 @@
-/* exported mica */
 'use strict';
 
-var mica = angular.module('mica', [
-  'ngObiba',
-  'ngRoute',
-  'ngSanitize',
-  'ngResource',
-  'ui.bootstrap',
-  'obiba.form',
-  'obiba.graphics',
-  'obiba.comments',
-  'angularUtils.directives.dirPagination',
-  'pascalprecht.translate',
-  'tmh.dynamicLocale',
-  'ngObibaMica'
-]);
+// Taxonomy sidebar menu
+Vue.component('taxonomy-menu', {
+  props: ['taxonomy'],
+  template: '<li class="nav-item"><a href="#" class="nav-link" :title="taxonomy.description[0].text" v-on:click="$emit(taxonomy.name)"><i class="far fa-circle nav-icon"></i><p>{{ taxonomy.title[0].text }}</p></a></li>'
+});
 
-mica.search = angular.module('mica.search', [
-  'ngObiba',
-  'ngRoute',
-  'ngSanitize',
-  'ngResource',
-  'ui.bootstrap',
-  'obiba.form',
-  'obiba.graphics',
-  'obiba.comments',
-  'angularUtils.directives.dirPagination',
-  'pascalprecht.translate',
-  'tmh.dynamicLocale',
-  'ngObibaMica',
-  'obiba.mica.search'
-]);
-
-mica.search
-  .factory('MicaConfigResource', ['$resource',
-    function ($resource) {
-      return $resource('ws/config', {}, {
-        'get': {method: 'GET'}
-      });
-    }])
-  .provider('SessionProxy',
-    function () {
-      function Proxy(user) {
-        var real = {login: user.name, roles: user.roles, profile: user.data || null};
-
-        this.update = function (value) {
-          real = value;
-        };
-
-        this.login = function () {
-          return real.login;
-        };
-
-        this.roles = function () {
-          return real.roles || [];
-        };
-
-        this.profile = function () {
-          return real.profile;
-        };
-      }
-
-      this.$get = function () {
-        return new Proxy({
-          name: 'anonymous',
-          roles: ['mica-user'],
-          data: {}
-        });
-      };
-    })
-  .run([
-    'GraphicChartsConfig',
-    function (GraphicChartsConfig) {
-      // TODO
-      GraphicChartsConfig.setOptions({});
-    }])
-  .factory('MicaConfigResource', ['$resource',
-    function ($resource) {
-      return $resource('../ws/config', {}, {
-        'get': {method: 'GET'}
-      });
-    }])
-  .factory('options', [function () {
-    const FIELDS_TO_FILTER = ['name', 'title', 'description', 'keywords'];
-    const QUERY_TARGETS = {
-      NETWORK: 'network',
-      STUDY: 'study',
-      DATASET: 'dataset',
-      VARIABLE: 'variable',
-      TAXONOMY: 'taxonomy'
-    };
-    const DISPLAY_TYPES = {
-      LIST: 'list',
-      COVERAGE: 'coverage',
-      GRAPHICS: 'graphics'
-    };
-
+new Vue({
+  el: '#search-criteria',
+  //el: '#app',
+  data() {
     return {
-      searchLayout: 'layout1',
-      taxonomyPanelOptions: {
-        network: {
-          taxonomies: {'Mica_network': {trKey: 'properties'}}
+      criteriaMenu: {
+        items: {
+          variable: {
+            icon: 'ion ion-pie-graph',
+            title: 'Variables',
+            menus: []
+          },
+          dataset: {
+            icon: 'ion ion-grid',
+            title: 'Datasets',
+            menus: []
+          },
+          study: {
+            icon: 'ion ion-folder',
+            title: 'Studies',
+            menus: []
+          },
+          network: {
+            icon: 'ion ion-filing',
+            title: 'Networks',
+            menus: []
+          },
         },
-        study: {
-          taxonomies: {'Mica_study': {trKey: 'properties'}}
-        },
-        dataset: {
-          taxonomies: {'Mica_dataset': {trKey: 'properties'}}
-        },
-        variable: {
-          taxonomies: {
-            'Mica_variable': {trKey: 'properties'}
-          }
-        },
-        fieldsToFilter: FIELDS_TO_FILTER
+        order: []
       },
-      obibaListOptions: {
-        countCaption: true,
-        searchForm: true,
-        supplInfoDetails: true,
-        trimmedDescription: true
-      },
-      targetTabsOrder: [QUERY_TARGETS.VARIABLE, QUERY_TARGETS.DATASET, QUERY_TARGETS.STUDY, QUERY_TARGETS.NETWORK],
-      searchTabsOrder: [DISPLAY_TYPES.LIST, DISPLAY_TYPES.COVERAGE, DISPLAY_TYPES.GRAPHICS],
-      resultTabsOrder: [QUERY_TARGETS.VARIABLE, QUERY_TARGETS.DATASET, QUERY_TARGETS.STUDY, QUERY_TARGETS.NETWORK],
-      showAllFacetedTaxonomies: true,
-      showFacetTermsWithZeroCount: false,
-      showSearchBox: true,
-      showSearchBrowser: true,
-      showCopyQuery: true,
-      showSearchRefreshButton: false,
-      variableTaxonomiesOrder: [],
-      studyTaxonomiesOrder: [],
-      datasetTaxonomiesOrder: [],
-      networkTaxonomiesOrder: [],
-      hideNavigate: [],
-      hideSearch: ['studyId', 'dceId', 'datasetId', 'networkId'],
-      variables: {
-        showSearchTab: true,
-        listPageSize: 20,
-        variablesColumn: {
-          showVariablesTypeColumn: true,
-          showVariablesStudiesColumn: true,
-          showVariablesDatasetsColumn: true,
-          showDatasetsStudiesColumn: true,
-          showDatasetsVariablesColumn: true
-        },
-        fields: [
-          'attributes.label.*',
-          'variableType',
-          'datasetId',
-          'datasetAcronym'
-        ],
-        annotationTaxonomies: [
-          'Mlstr_area'
-        ],
-        showCart: true
-      },
-      datasets: {
-        showSearchTab: true,
-        listPageSize: 20,
-        showDatasetsSearchFilter: true,
-        datasetsColumn: {
-          showDatasetsAcronymColumn: true,
-          showDatasetsTypeColumn: true,
-          showDatasetsNetworkColumn: true,
-          showDatasetsStudiesColumn: true,
-          showDatasetsVariablesColumn: true
-        },
-        fields: [
-          'acronym.*',
-          'name.*',
-          'variableType',
-          'studyTable.studyId',
-          'studyTable.project',
-          'studyTable.table',
-          'studyTable.populationId',
-          'studyTable.dataCollectionEventId',
-          'harmonizationTable.studyId',
-          'harmonizationTable.project',
-          'harmonizationTable.table',
-          'harmonizationTable.populationId'
-        ]
-      },
-      studies: {
-        showSearchTab: true,
-        listPageSize: 20,
-        showStudiesSearchFilter: true,
-        studiesColumn: {
-          showStudiesTypeColumn: true,
-          showStudiesDesignColumn: true,
-          showStudiesQuestionnaireColumn: true,
-          showStudiesPmColumn: true,
-          showStudiesBioColumn: true,
-          showStudiesOtherColumn: true,
-          showStudiesParticipantsColumn: true,
-          showStudiesNetworksColumn: true,
-          showStudiesStudyDatasetsColumn: true,
-          showStudiesHarmonizationDatasetsColumn: true,
-          showStudiesVariablesColumn: false,
-          showStudiesStudyVariablesColumn: true,
-          showStudiesDataschemaVariablesColumn: true
-        },
-        fields: [
-          'acronym.*',
-          'name.*',
-          'model.methods.design',
-          'populations.dataCollectionEvents.model.dataSources',
-          'model.numberOfParticipants.participant'
-        ]
-      },
-      networks: {
-        showSearchTab: true,
-        listPageSize: 20,
-        networksColumn: {
-          showNetworksStudiesColumn: true,
-          showNetworksStudyDatasetColumn: true,
-          showNetworksHarmonizationDatasetColumn: true,
-          showNetworksVariablesColumn: false,
-          showNetworksStudyVariablesColumn: true,
-          showNetworksDataschemaVariablesColumn: true
-        },
-        fields: [
-          'acronym.*',
-          'name.*',
-          'studyIds'
-        ]
-      },
-      coverage: {
-        total: {
-          showInHeader: true,
-          showInFooter: false
-        },
-        groupBy: {
-          study: true,
-          dce: true,
-          dataset: true
-        }
-      }
+      taxonomies: {},
+      initialized: false
     };
-    //return ngObibaMicaSearch.getOptionsAsyn();
-  }])
-  .config(['$routeProvider', '$locationProvider', 'ObibaServerConfigResourceProvider', 'ngObibaMicaSearchProvider', 'ngObibaMicaUrlProvider', 'tmhDynamicLocaleProvider', '$translateProvider',
-    function ($routeProvider, $locationProvider, ObibaServerConfigResourceProvider, ngObibaMicaSearchProvider, ngObibaMicaUrlProvider, tmhDynamicLocaleProvider, $translateProvider) {
-
-      // Initialize angular-translate
-      $translateProvider
-        .useStaticFilesLoader({
-          prefix: 'ws/config/i18n/',
-          suffix: '.json'
-        })
-        .registerAvailableLanguageKeys(['en', 'fr'], {
-          'en_*': 'en',
-          'fr_*': 'fr',
-          '*': 'en'
-        })
-        .determinePreferredLanguage()
-        .fallbackLanguage('en')
-        .useCookieStorage()
-        .useSanitizeValueStrategy('escaped');
-
-      tmhDynamicLocaleProvider.localeLocationPattern('../bower_components/angular-i18n/angular-locale_{{locale}}.js');
-      tmhDynamicLocaleProvider.useCookieStorage('NG_TRANSLATE_LANG_KEY');
-
-      // This will be used to delay the loading of the search config until the options are all resolved; the result is
-      // injected to the SearchController.
-      /*var optionsResolve = ['ngObibaMicaSearch', function (ngObibaMicaSearch) {
-         return ngObibaMicaSearch.getOptionsAsyn();
-       }];
-
-       $routeProvider
-         .when('/', {
-           templateUrl: '../bower_components/ng-obiba-mica/src/search/views/search-layout.html',
-           controller: 'SearchController',
-           reloadOnSearch: false,
-           resolve: {
-             options: optionsResolve
-           }
-         });*/
-
-      // TODO
-
-      //ngObibaMicaSearchProvider.initialize();
-
-      $locationProvider.hashPrefix('');
-
-      ObibaServerConfigResourceProvider.setFactory(
-        ['MicaConfigResource', function (MicaConfigResource) {
-          return {get: MicaConfigResource.get};
-        }]
-      );
-
-      ngObibaMicaUrlProvider.setUrl('TaxonomiesSearchResource', '../ws/taxonomies/_search');
-      ngObibaMicaUrlProvider.setUrl('TaxonomiesResource', '../ws/taxonomies/_filter');
-      ngObibaMicaUrlProvider.setUrl('TaxonomyResource', '../ws/taxonomy/:taxonomy/_filter');
-      ngObibaMicaUrlProvider.setUrl('VocabularyResource', '../ws/taxonomy/:taxonomy/vocabulary/:vocabulary/_filter');
-      ngObibaMicaUrlProvider.setUrl('JoinQuerySearchResource', 'ws/:type/_rql');
-      ngObibaMicaUrlProvider.setUrl('JoinQuerySearchCsvResource', 'ws/:type/_rql_csv?query=:query');
-      ngObibaMicaUrlProvider.setUrl('JoinQuerySearchCsvReportResource', 'ws/:type/_report?query=:query');
-      ngObibaMicaUrlProvider.setUrl('JoinQuerySearchCsvReportByNetworkResource', 'ws/:type/_report_by_network?networkId=:networkId&locale=:locale');
-      ngObibaMicaUrlProvider.setUrl('JoinQueryCoverageResource', 'ws/variables/_coverage');
-      ngObibaMicaUrlProvider.setUrl('JoinQueryCoverageDownloadResource', 'ws/variables/_coverage_download?query=:query');
-      // TODO more URL override
-
-    }]);
+  },
+  mounted() {
+    axios
+      .get('../ws/taxonomy/Mica_taxonomy/_filter?target=taxonomy')
+      .then(response => {
+        //console.dir(response.data.vocabularies);
+        let vocabularies = response.data.vocabularies;
+        for (let group of vocabularies) {
+          //this.criteriaMenu.order.push(group.name);
+          this.criteriaMenu.items[group.name].title = group.title[0].text;
+          switch(group.name) {
+            case 'variable':
+              // TODO handle multi level
+              this.criteriaMenu.items.variable.menus = group.terms[0].terms;
+              break;
+            case 'dataset':
+            case 'study':
+            case 'network':
+              this.criteriaMenu.items[group.name].menus = group.terms;
+              break;
+          }
+          if (this.criteriaMenu.items[group.name].menus && this.criteriaMenu.items[group.name].menus.length>0) {
+            this.criteriaMenu.order.push(group.name);
+          }
+        }
+        // then load the taxonomies
+        for (let name of this.criteriaMenu.order) {
+          axios
+            .get('../ws/taxonomies/_filter?target=' + name)
+            .then(response => {
+              for (let taxo of response.data) {
+                this.taxonomies[taxo.name] = taxo;
+              }
+            });
+        }
+        this.initialized = true;
+      });
+  }
+});
