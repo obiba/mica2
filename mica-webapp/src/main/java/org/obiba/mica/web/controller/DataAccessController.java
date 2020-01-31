f2cc8b15da14c7e51a979972aca226913edbdc41
Yannick Marcon <yannick.marcon@obiba.org>
2020-01-31 23:40:03 +0100

    #3833 DAR list and invididual, first commit

--- mica-webapp/src/main/java/org/obiba/mica/web/controller/DataAccessController.java
+++ mica-webapp/src/main/java/org/obiba/mica/web/controller/DataAccessController.java
@@ -1,16 +1,38 @@
 package org.obiba.mica.web.controller;
 
+import com.google.common.collect.Maps;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+import org.obiba.mica.access.domain.DataAccessRequest;
+import org.obiba.mica.access.service.DataAccessRequestService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.inject.Inject;
+import java.util.Map;
+
 @Controller
 public class DataAccessController {
 
+  @Inject
+  private DataAccessRequestService dataAccessRequestService;
+
   @GetMapping("/data-access/{id}")
   public ModelAndView get(@PathVariable String id) {
-    return new ModelAndView("data-access");
+    Subject subject = SecurityUtils.getSubject();
+    if (subject.isAuthenticated()) {
+      Map<String, Object> params = Maps.newHashMap();
+      params.put("dar", getDataAccessRequest(id));
+      return new ModelAndView("data-access", params);
+    } else {
+      return new ModelAndView("redirect:signin?redirect=data-access/" + id);
+    }
+  }
+
+  private DataAccessRequest getDataAccessRequest(String id) {
+    return dataAccessRequestService.findById(id);
   }
 
   @GetMapping("/data-access/{id}/form")
