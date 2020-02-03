f6f77f1950719bbaf3a94774c85db89729b6b16b
Yannick Marcon <yannick.marcon@obiba.org>
2020-02-03 18:33:52 +0100

    #3833 Attempt to integrate schema-form in DAR form page

--- mica-webapp/src/main/java/org/obiba/mica/web/controller/DataAccessController.java
+++ mica-webapp/src/main/java/org/obiba/mica/web/controller/DataAccessController.java
@@ -3,21 +3,41 @@ package org.obiba.mica.web.controller;
 import com.google.common.collect.Maps;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
+import org.obiba.core.translator.JsonTranslator;
+import org.obiba.core.translator.PrefixedValueTranslator;
+import org.obiba.core.translator.TranslationUtils;
+import org.obiba.core.translator.Translator;
 import org.obiba.mica.access.domain.DataAccessRequest;
 import org.obiba.mica.access.service.DataAccessRequestService;
+import org.obiba.mica.micaConfig.NoSuchDataAccessFormException;
+import org.obiba.mica.micaConfig.domain.DataAccessForm;
+import org.obiba.mica.micaConfig.service.DataAccessFormService;
+import org.obiba.mica.micaConfig.service.MicaConfigService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.inject.Inject;
+import javax.ws.rs.QueryParam;
 import java.util.Map;
+import java.util.Optional;
 
 @Controller
 public class DataAccessController extends BaseController {
 
   @Inject
   private DataAccessRequestService dataAccessRequestService;
+
+  @Inject
+  DataAccessFormService dataAccessFormService;
+
+  @Inject
+  private MicaConfigService micaConfigService;
 
   @GetMapping("/data-access/{id}")
   public ModelAndView get(@PathVariable String id) {
@@ -33,12 +53,15 @@ public class DataAccessController extends BaseController {
   }
 
   @GetMapping("/data-access-form/{id}")
-  public ModelAndView getForm(@PathVariable String id) {
+  public ModelAndView getForm(@PathVariable String id, @CookieValue(value = "NG_TRANSLATE_LANG_KEY", required = false, defaultValue = "en") String locale,
+                              @RequestParam(value = "language", required = false) String language) {
     Subject subject = SecurityUtils.getSubject();
     if (subject.isAuthenticated()) {
+      DataAccessRequest dar = getDataAccessRequest(id);
       Map<String, Object> params = newParameters();
-      params.put("dar", getDataAccessRequest(id));
+      params.put("dar", dar);
       params.put("pathPrefix", "../..");
+      addDataAccessForm(params, dar.getContent(), language == null ? locale : language);
       return new ModelAndView("data-access-form", params);
     } else {
       return new ModelAndView("redirect:signin?redirect=data-access-form/" + id);
@@ -47,6 +70,12 @@ public class DataAccessController extends BaseController {
 
   private DataAccessRequest getDataAccessRequest(String id) {
     return dataAccessRequestService.findById(id);
+  }
+
+  private void addDataAccessForm(Map<String, Object> params, String model, String locale) {
+    Optional<DataAccessForm> d = dataAccessFormService.find();
+    if(!d.isPresent()) throw NoSuchDataAccessFormException.withDefaultMessage();
+    params.put("form", new SchemaForm(d.get(), model, locale));
   }
 
   @GetMapping("/data-access/{id}/amendment/{aid}/form")
@@ -64,4 +93,33 @@ public class DataAccessController extends BaseController {
     return new ModelAndView("data-access-history");
   }
 
+  public class SchemaForm {
+
+    private final String schema;
+    private final String definition;
+    private final String model;
+
+    private SchemaForm(DataAccessForm form, String model, String locale) {
+      String lang = locale == null ? "en" : locale.replaceAll("\"", "");
+      Translator translator = JsonTranslator.buildSafeTranslator(() -> micaConfigService.getTranslations(lang, false));
+      translator = new PrefixedValueTranslator(translator);
+
+      TranslationUtils translationUtils = new TranslationUtils();
+      this.schema = translationUtils.translate(form.getSchema(), translator).replaceAll("col-xs-", "col-");
+      this.definition = translationUtils.translate(form.getDefinition(), translator).replaceAll("col-xs-", "col-");
+      this.model = model;
+    }
+
+    public String getSchema() {
+      return schema;
+    }
+
+    public String getDefinition() {
+      return definition;
+    }
+
+    public String getModel() {
+      return model;
+    }
+  }
 }
