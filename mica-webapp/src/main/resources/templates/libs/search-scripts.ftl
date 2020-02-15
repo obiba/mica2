e89d0e539273d11e66926b12c287c9ea5b0fd46c
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2020-02-15 18:55:53 -0500

    Ramin templates (#3844)
    * Added setup to install static pages JS deps + demo code for vars search result.
    
    * Added rql lib.

--- mica-webapp/src/main/resources/templates/libs/search-scripts.ftl
+++ mica-webapp/src/main/resources/templates/libs/search-scripts.ftl
@@ -1,4 +1,5 @@
 <!-- Mica Search and dependencies -->
 
 <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+<script src="../js/libs/node_modules/vue-obiba-search-result/dist/VueObibaSearchResult.umd.js"></script>
 <script src="../js/mica-search.js"></script>
--- mica-webapp/src/main/webapp/js/mica-search.js
+++ mica-webapp/src/main/webapp/js/mica-search.js
@@ -25,6 +25,15 @@ const EventBus = new Vue({
   }
 });
 
+const DataTableDefaults = {
+  searching: false,
+  ordering: false,
+  lengthMenu: [10, 20, 50, 100],
+  pageLength: 20,
+  // Paginatiom on top (the bottom one still remains)
+  dom: "<'row'<'col-sm-3'l><'col-sm-3'f><'col-sm-6'p>><'row'<'col-sm-12'tr>><'row'<'col-sm-5'i><'col-sm-7'p>>" // pager top
+};
+
 //
 // Search criteria Vue
 // sidebar menus for taxonomy selection. Terms selection is delegated to the main app (query builder)
@@ -105,7 +114,7 @@ new Vue({
 //
 
 $('#variables-tab').click(function(){
-  EventBus.$emit('query-type-selection', 'variables-list');
+  EventBus.$emit('query-type-selection', {type: 'variables-list'});
 });
 
 $('#datasets-tab').click(function(){
@@ -132,6 +141,100 @@ $('#graphics-tab').click(function(){
   EventBus.$emit('query-type-selection', 'graphics');
 });
 
+Vue.use(VueObibaSearchResult.VariablesResult, {
+  mixin: {
+    methods: {
+      getEventBus: function() {
+        return EventBus;
+      },
+      registerDataTable: function (tableId, options) {
+        const mergedOptions = Object.assign(options, DataTableDefaults);
+        return $('#' + tableId).DataTable(mergedOptions);
+      }
+    }
+  }
+});
+
+//
+// Querybuilder Vue
+// main app that orchestrates the query display, criteria selection, query execution and dispatch of the results
+//
+
+new Vue({
+  el: '#query-builder',
+  data() {
+    return {
+      taxonomies: {},
+      message: '',
+      queryType: 'variables-list',
+      lastList: ''
+    };
+  },
+  methods: {
+    // show a modal with all the vocabularies/terms of the selected taxonomy
+    // initialized by the query terms and update/trigger the query on close
+    onTaxonomySelection: function(payload) {
+      this.message = '[' + payload + '] ' + this.taxonomies[payload].title[0].text + ': ';
+      this.message = this.message + this.taxonomies[payload].vocabularies.map(voc => voc.title[0].text).join(', ');
+    },
+    // set the type of query to be executed, on result component selection
+    onQueryTypeSelection: function(payload) {
+      if (payload === 'lists') {
+        this.queryType = this.lastList;
+      } else if (typeof payload  === 'object') {
+        const from = payload.from ||  0;
+        const size = payload.size || 20;
+        this.executeQuery(payload, 'variables', `locale(en),variable(limit(${from},${size}),fields((attributes.label.*,variableType,datasetId,datasetAcronym,attributes.Mlstr_area*)),sort(variableType,containerId,populationWeight,dataCollectionEventWeight,datasetId,index,name))`);
+      } else {
+        this.queryType = payload;
+        if (payload.endsWith('-list')) {
+          this.lastList = payload;
+        }
+      }
+      this.onExecuteQuery();
+    },
+    onExecuteQuery: function() {
+      console.log('Executing ' + this.queryType + ' query ...');
+      EventBus.$emit(this.queryType, 'I am the result of a ' + this.queryType + ' query');
+    },
+    executeQuery: function(payload, target, query) {
+      axios
+        .get(`../ws/${target}/_rql?query=${query}`)
+        .then(response => {
+          EventBus.$emit('query-result-received', response.data);
+        });
+    }
+  },
+  beforeMount() {
+    console.log('Before mounted QueryBuilder');
+  },
+  mounted() {
+    console.log('Mounted QueryBuilder');
+    EventBus.register('taxonomy-selection', this.onTaxonomySelection);
+    EventBus.register('query-type-selection', this.onQueryTypeSelection);
+
+    // fetch the configured search criteria, in the form of a taxonomy of taxonomies
+    axios
+      .get('../ws/taxonomy/Mica_taxonomy/_filter?target=taxonomy')
+      .then(response => {
+        let targets = response.data.vocabularies;
+        EventBus.$emit('mica-taxonomy', targets);
+
+        for (let target of targets) {
+          // then load the taxonomies
+          axios
+            .get('../ws/taxonomies/_filter?target=' + target.name)
+            .then(response => {
+              for (let taxo of response.data) {
+                this.taxonomies[taxo.name] = taxo;
+              }
+            });
+        }
+      });
+    this.onExecuteQuery();
+  }
+});
+
 new Vue({
   el: '#list-variables',
   data() {
@@ -145,6 +248,10 @@ new Vue({
       this.count++;
       this.result = payload + ' ' + this.count;
     }
+  },
+  beforeMount() {
+    console.log('Before mounted List Variables');
+    EventBus.$emit('query-type-selection', {type: 'variables-list', from: 0, size: DataTableDefaults.pageLength});
   },
   mounted() {
     EventBus.register('variables-list', this.onResult);
@@ -246,66 +353,4 @@ new Vue({
   }
 });
 
-//
-// Querybuilder Vue
-// main app that orchestrates the query display, criteria selection, query execution and dispatch of the results
-//
 
-new Vue({
-  el: '#query-builder',
-  data() {
-    return {
-      taxonomies: {},
-      message: '',
-      queryType: 'variables-list',
-      lastList: ''
-    };
-  },
-  methods: {
-    // show a modal with all the vocabularies/terms of the selected taxonomy
-    // initialized by the query terms and update/trigger the query on close
-    onTaxonomySelection: function(payload) {
-      this.message = '[' + payload + '] ' + this.taxonomies[payload].title[0].text + ': ';
-      this.message = this.message + this.taxonomies[payload].vocabularies.map(voc => voc.title[0].text).join(', ');
-    },
-    // set the type of query to be executed, on result component selection
-    onQueryTypeSelection: function(payload) {
-      if (payload === 'lists') {
-        this.queryType = this.lastList;
-      } else {
-        this.queryType = payload;
-        if (payload.endsWith('-list')) {
-          this.lastList = payload;
-        }
-      }
-      this.onExecuteQuery();
-    },
-    onExecuteQuery: function() {
-      console.log('Executing ' + this.queryType + ' query ...');
-      EventBus.$emit(this.queryType, 'I am the result of a ' + this.queryType + ' query');
-    }
-  },
-  mounted() {
-    EventBus.register('taxonomy-selection', this.onTaxonomySelection);
-    EventBus.register('query-type-selection', this.onQueryTypeSelection);
-    // fetch the configured search criteria, in the form of a taxonomy of taxonomies
-    axios
-      .get('../ws/taxonomy/Mica_taxonomy/_filter?target=taxonomy')
-      .then(response => {
-        let targets = response.data.vocabularies;
-        EventBus.$emit('mica-taxonomy', targets);
-
-        for (let target of targets) {
-        // then load the taxonomies
-        axios
-          .get('../ws/taxonomies/_filter?target=' + target.name)
-          .then(response => {
-            for (let taxo of response.data) {
-              this.taxonomies[taxo.name] = taxo;
-            }
-          });
-        }
-      });
-    this.onExecuteQuery();
-  }
-});
