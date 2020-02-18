0bd74381dde27f337a1370ec0cf224090bb29ae1
Yannick Marcon <yannick.marcon@obiba.org>
2020-02-18 10:41:25 +0100

    #3833 Handle case there are no index

--- mica-webapp/src/main/java/org/obiba/mica/web/controller/NetworksController.java
+++ mica-webapp/src/main/java/org/obiba/mica/web/controller/NetworksController.java
@@ -1,5 +1,7 @@
 package org.obiba.mica.web.controller;
 
+import com.google.common.collect.Lists;
+import org.obiba.mica.network.domain.Network;
 import org.obiba.mica.network.service.PublishedNetworkService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.inject.Inject;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -19,10 +22,16 @@ public class NetworksController extends BaseController {
   @GetMapping("/networks")
   public ModelAndView list() {
 
-    Map<String, Object> params = new HashMap<String, Object>();
-    params.put("networks", publishedNetworkService.findAll().stream()
-      .filter(n -> isAccessible("/network", n.getId()))
-      .collect(Collectors.toList()));
+    Map<String, Object> params = newParameters();
+    List<Network> networks;
+    try {
+      networks = publishedNetworkService.findAll().stream()
+        .filter(n -> isAccessible("/network", n.getId()))
+        .collect(Collectors.toList());
+    } catch (Exception e) {
+      networks = Lists.newArrayList();
+    }
+    params.put("networks", networks);
 
     return new ModelAndView("networks", params);
   }
