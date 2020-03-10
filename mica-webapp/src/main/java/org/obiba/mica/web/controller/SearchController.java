916304b52c4677785965e528c1f5a3265d2b954e
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2020-03-10 08:03:26 -0400

    Removed the cache in the SearchController, config is already in EhCache.

--- mica-webapp/src/main/java/org/obiba/mica/web/controller/SearchController.java
+++ mica-webapp/src/main/java/org/obiba/mica/web/controller/SearchController.java
@@ -3,10 +3,7 @@ package org.obiba.mica.web.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Maps;
-import com.google.common.eventbus.Subscribe;
-import org.obiba.mica.micaConfig.event.MicaConfigUpdatedEvent;
 import org.obiba.mica.micaConfig.service.MicaConfigService;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,8 +15,6 @@ import java.util.Map;
 public class SearchController {
 
   private MicaConfigService micaConfigService;
-
-  private String cachedMicaConfigJson = null;
 
   @Inject
   public SearchController(MicaConfigService micaConfigService) {
@@ -33,24 +28,14 @@ public class SearchController {
     return new ModelAndView("search", params);
   }
 
-  @Async
-  @Subscribe
-  public void micaConfigUpdated(MicaConfigUpdatedEvent event) {
-    cachedMicaConfigJson = null;
-    initMicaConfigAsJson();
-  }
-
-  private void initMicaConfigAsJson() {
-    if (cachedMicaConfigJson != null) return;
-
+  private String getMicaConfigAsJson() {
     ObjectMapper mapper = new ObjectMapper();
     try {
-      cachedMicaConfigJson = mapper.writeValueAsString(micaConfigService.getConfig());
+      return mapper.writeValueAsString(micaConfigService.getConfig());
     } catch (JsonProcessingException ignore) {
     }
+
+    return "{}";
   }
 
-  private String getMicaConfigAsJson() {
-    return cachedMicaConfigJson == null ? "{}": cachedMicaConfigJson;
-  }
 }
