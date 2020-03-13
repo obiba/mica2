6872f2e1ecc41f47a6a7718b7dff3522910c4eef
Yannick Marcon <yannick.marcon@obiba.org>
2020-03-13 16:22:09 +0100

    Mica config Json only in Search page (breaks page redirect)

--- mica-webapp/src/main/java/org/obiba/mica/web/controller/SearchController.java
+++ mica-webapp/src/main/java/org/obiba/mica/web/controller/SearchController.java
@@ -1,15 +1,37 @@
 package org.obiba.mica.web.controller;
 
+import com.googlecode.protobuf.format.JsonFormat;
+import org.obiba.mica.micaConfig.service.MicaConfigService;
+import org.obiba.mica.web.model.Dtos;
+import org.obiba.mica.web.model.Mica;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.inject.Inject;
+
 @Controller
 public class SearchController {
 
+  @Inject
+  private MicaConfigService micaConfigService;
+
+  @Inject
+  private Dtos dtos;
+
   @GetMapping("/search")
   public ModelAndView search() {
-    return new ModelAndView("search");
+    ModelAndView mv = new ModelAndView("search");
+    mv.getModel().put("configJson", getMicaConfigAsJson());
+    return mv;
   }
 
+  private String getMicaConfigAsJson() {
+    try {
+      Mica.MicaConfigDto dto = dtos.asDto(micaConfigService.getConfig());
+      return JsonFormat.printToString(dto);
+    } catch (Exception ignore) {
+    }
+    return "{}";
+  }
 }
