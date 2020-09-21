d74ad97de5ddc9c15990f5b60450a227b32c11c7
Yannick Marcon <yannick.marcon@obiba.org>
2020-09-21 13:31:37 +0200

    #4012 Fix missing cart set title in variables taxo

--- mica-core/src/main/java/org/obiba/mica/micaConfig/service/helper/SetsAggregationMetaDataHelper.java
+++ mica-core/src/main/java/org/obiba/mica/micaConfig/service/helper/SetsAggregationMetaDataHelper.java
@@ -2,8 +2,13 @@ package org.obiba.mica.micaConfig.service.helper;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import javax.inject.Inject;
+
+import com.google.common.base.Strings;
+import org.obiba.core.translator.JsonTranslator;
+import org.obiba.core.translator.Translator;
 import org.obiba.mica.core.domain.DocumentSet;
 import org.obiba.mica.core.domain.LocalizedString;
 import org.obiba.mica.dataset.service.VariableSetService;
@@ -29,12 +34,19 @@ public class SetsAggregationMetaDataHelper extends AbstractIdAggregationMetaData
   public Map<String, LocalizedMetaData> getSetIds() {
     List<String> locales = micaConfigService.getLocales();
 
+    Map<String, Translator> translators = locales.stream()
+      .collect(Collectors.toMap(Function.identity(), locale -> JsonTranslator.buildSafeTranslator(() -> micaConfigService.getTranslations(locale, false))));
+
     return variableSetService.getAll().stream()
       .collect(Collectors.toMap(
         DocumentSet::getId,
         set -> {
           LocalizedString title = new LocalizedString();
-          locales.forEach(locale -> title.forLanguageTag(locale, set.getName()));
+          locales.forEach(locale -> {
+            String setName = Strings.isNullOrEmpty(set.getName()) ?
+              translators.get(locale).translate("sets.cart.title") : set.getName();
+            title.forLanguageTag(locale, setName);
+          });
           return new LocalizedMetaData(title, new LocalizedString(), set.getType());
         }
       ));
