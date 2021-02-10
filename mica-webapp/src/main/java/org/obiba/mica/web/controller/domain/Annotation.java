7b25add4ccaae20195924f30a9009d219caac255
Yannick Marcon <yannick.marcon@obiba.org>
2021-02-10 10:14:06 +0100

    Revert "Return algorithm value as LocalizedString (#4135)" (#4136)
    This reverts commit 05ec230c375c4e51eda473f237a1ce388d000998.

--- mica-webapp/src/main/java/org/obiba/mica/web/controller/domain/Annotation.java
+++ mica-webapp/src/main/java/org/obiba/mica/web/controller/domain/Annotation.java
@@ -45,10 +45,6 @@ public class Annotation {
     return attribute.getValues().get("und");
   }
 
-  public LocalizedString getTermValues() {
-    return attribute.getValues();
-  }
-
   public LocalizedString getTermTitle() {
     Vocabulary vocabulary = getVocabulary();
     if (vocabulary.hasTerms() && vocabulary.hasTerm(getTermName())) {
