bfa3b8217e4246ca65a37c2b256c1a17b5972f73
Yannick Marcon <yannick.marcon@obiba.org>
2021-10-04 23:22:37 +0200

    Data access config upgrade step rewritten in Java (#4236)
    * Data access config upgrade step rewritten in Java
    
    * remove original _id

--- mica-core/src/main/java/org/obiba/mica/core/upgrade/Mica460Upgrade.java
+++ mica-core/src/main/java/org/obiba/mica/core/upgrade/Mica460Upgrade.java
@@ -1,7 +1,8 @@
 package org.obiba.mica.core.upgrade;
 
 import com.google.common.eventbus.EventBus;
-import org.obiba.mica.micaConfig.event.TaxonomiesUpdatedEvent;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
 import org.obiba.runtime.Version;
 import org.obiba.runtime.upgrade.UpgradeStep;
 import org.slf4j.Logger;
@@ -40,78 +41,76 @@ public class Mica460Upgrade implements UpgradeStep {
     logger.info("Executing Mica upgrade to version 4.6.0");
     try {
       logger.info("Moving data access config out of data access form");
-      mongoTemplate.execute(db -> db.eval(initDataAccessConfig()));
-      mongoTemplate.execute(db -> db.eval(cleanDataAccessForm()));
+      DBObject dataAccessForm = mongoTemplate.execute(db -> db.getCollection("dataAccessForm").find().next());
+      insertDataAccessConfig(dataAccessForm);
+      updateDataAccessForm(dataAccessForm);
     } catch (RuntimeException e) {
       logger.error("Error occurred when trying to execute removeSetsVocabulariesTerms.", e);
     }
   }
 
-  private String initDataAccessConfig() {
-    return "db.getCollection('dataAccessForm').find({})\n" +
-      ".map(function(doc) {\n" +
-      "    delete doc.pdfTemplates;\n" +
-      "    delete doc.pdfDownloadType;\n" +
-      "    delete doc.csvExportFormat;\n" +
-      "    delete doc.properties;\n" +
-      "    delete doc.titleFieldPath;\n" +
-      "    delete doc.summaryFieldPath;\n" +
-      "    delete doc.endDateFieldPath;\n" +
-      "    delete doc.schema;\n" +
-      "    delete doc.definition;\n" +
-      "    doc.version = 1;\n" +
-      "    doc._class = 'org.obiba.mica.micaConfig.domain.DataAccessConfig';\n" +
-      "    doc._id = new ObjectId();\n" +
-      "    return doc;\n" +
-      "    })\n" +
-      ".forEach(function(doc) { \n" +
-      "    db.getCollection('dataAccessConfig').save(doc);\n" +
-      "    })";
+  private void insertDataAccessConfig(DBObject dataAccessForm) {
+    DBObject dataAccessConfig = new BasicDBObject();
+    dataAccessConfig.putAll(dataAccessForm);
+    dataAccessConfig.removeField("pdfTemplates");
+    dataAccessConfig.removeField("pdfDownloadType");
+    dataAccessConfig.removeField("csvExportFormat");
+    dataAccessConfig.removeField("properties");
+    dataAccessConfig.removeField("titleFieldPath");
+    dataAccessConfig.removeField("summaryFieldPath");
+    dataAccessConfig.removeField("endDateFieldPath");
+    dataAccessConfig.removeField("schema");
+    dataAccessConfig.removeField("definition");
+    dataAccessConfig.removeField("_id");
+    dataAccessConfig.put("version", 1);
+    dataAccessConfig.put("_class", "org.obiba.mica.micaConfig.domain.DataAccessConfig");
+
+    mongoTemplate.execute(db -> {
+      if (!db.collectionExists("dataAccessConfig"))
+        db.createCollection("dataAccessConfig", new BasicDBObject());
+      return db.getCollection("dataAccessConfig").save(dataAccessConfig);
+    });
   }
 
-  private String cleanDataAccessForm() {
-    return "db.getCollection('dataAccessForm').find({})\n" +
-      ".map(function(doc) {\n" +
-      "    delete doc.idLength;\n" +
-      "    delete doc.allowIdWithLeadingZeros;\n" +
-      "    delete doc.notifyCreated;\n" +
-      "    delete doc.notifySubmitted;\n" +
-      "    delete doc.notifyReviewed;\n" +
-      "    delete doc.notifyConditionallyApproved;\n" +
-      "    delete doc.notifyApproved;\n" +
-      "    delete doc.notifyRejected;\n" +
-      "    delete doc.notifyReopened;\n" +
-      "    delete doc.notifyCommented;\n" +
-      "    delete doc.notifyAttachment;\n" +
-      "    delete doc.notifyFinalReport;\n" +
-      "    delete doc.notifyIntermediateReport;\n" +
-      "    delete doc.createdSubject;\n" +
-      "    delete doc.submittedSubject;\n" +
-      "    delete doc.reviewedSubject;\n" +
-      "    delete doc.conditionallyApprovedSubject;\n" +
-      "    delete doc.approvedSubject;\n" +
-      "    delete doc.rejectedSubject;\n" +
-      "    delete doc.reopenedSubject;\n" +
-      "    delete doc.commentedSubject;\n" +
-      "    delete doc.attachmentSubject;\n" +
-      "    delete doc.finalReportSubject;\n" +
-      "    delete doc.intermediateReportSubject;\n" +
-      "    delete doc.withReview;\n" +
-      "    delete doc.withConditionalApproval;\n" +
-      "    delete doc.approvedFinal;\n" +
-      "    delete doc.rejectedFinal;\n" +
-      "    delete doc.predefinedActions;\n" +
-      "    delete doc.feasibilityEnabled;\n" +
-      "    delete doc.amendmentsEnabled;\n" +
-      "    delete doc.variablesEnabled;\n" +
-      "    delete doc.feasibilityVariablesEnabled;\n" +
-      "    delete doc.amendmentVariablesEnabled;\n" +
-      "    delete doc.daoCanEdit;\n" +
-      "    delete doc.nbOfDaysBeforeReport;\n" +
-      "    return doc;\n" +
-      "    })\n" +
-      ".forEach(function(doc) { \n" +
-      "    db.getCollection('dataAccessForm').save(doc);\n" +
-      "    })";
+  private void updateDataAccessForm(DBObject dataAccessForm) {
+    dataAccessForm.removeField("idLength");
+    dataAccessForm.removeField("allowIdWithLeadingZeros");
+    dataAccessForm.removeField("notifyCreated");
+    dataAccessForm.removeField("notifySubmitted");
+    dataAccessForm.removeField("notifyReviewed");
+    dataAccessForm.removeField("notifyConditionallyApproved");
+    dataAccessForm.removeField("notifyApproved");
+    dataAccessForm.removeField("notifyRejected");
+    dataAccessForm.removeField("notifyReopened");
+    dataAccessForm.removeField("notifyCommented");
+    dataAccessForm.removeField("notifyAttachment");
+    dataAccessForm.removeField("notifyFinalReport");
+    dataAccessForm.removeField("notifyIntermediateReport");
+    dataAccessForm.removeField("createdSubject");
+    dataAccessForm.removeField("submittedSubject");
+    dataAccessForm.removeField("reviewedSubject");
+    dataAccessForm.removeField("conditionallyApprovedSubject");
+    dataAccessForm.removeField("approvedSubject");
+    dataAccessForm.removeField("rejectedSubject");
+    dataAccessForm.removeField("reopenedSubject");
+    dataAccessForm.removeField("commentedSubject");
+    dataAccessForm.removeField("attachmentSubject");
+    dataAccessForm.removeField("finalReportSubject");
+    dataAccessForm.removeField("intermediateReportSubject");
+    dataAccessForm.removeField("withReview");
+    dataAccessForm.removeField("withConditionalApproval");
+    dataAccessForm.removeField("approvedFinal");
+    dataAccessForm.removeField("rejectedFinal");
+    dataAccessForm.removeField("predefinedActions");
+    dataAccessForm.removeField("feasibilityEnabled");
+    dataAccessForm.removeField("amendmentsEnabled");
+    dataAccessForm.removeField("variablesEnabled");
+    dataAccessForm.removeField("feasibilityVariablesEnabled");
+    dataAccessForm.removeField("amendmentVariablesEnabled");
+    dataAccessForm.removeField("daoCanEdit");
+    dataAccessForm.removeField("nbOfDaysBeforeReport");
+
+    mongoTemplate.execute(db -> db.getCollection("dataAccessForm").save(dataAccessForm));
   }
+
 }
