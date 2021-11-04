e0f7a142159ebd048f93d94676cc254caa7a3858
meek0 <cedricgfont@gmail.com>
2021-11-04 12:06:02 -0400

    person management to require draft study and network view and edit pe… (#4257)
    * person management to require draft study and network view and edit permissions
    
    * removed json package lock and less specific
    
    * fix person by network permission

--- mica-rest/src/main/java/org/obiba/mica/person/rest/PersonsResource.java
+++ mica-rest/src/main/java/org/obiba/mica/person/rest/PersonsResource.java
@@ -11,8 +11,11 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.obiba.mica.contact.event.IndexContactsEvent;
 import org.obiba.mica.core.service.PersonService;
+import org.obiba.mica.security.service.SubjectAclService;
+import org.obiba.mica.study.service.StudyService;
 import org.obiba.mica.web.model.Dtos;
 import org.obiba.mica.web.model.Mica.PersonDto;
 import org.springframework.stereotype.Component;
@@ -26,35 +29,50 @@ public class PersonsResource {
 
   private final PersonService personService;
 
+  private final SubjectAclService subjectAclService;
+
+  private final StudyService studyService;
+
   private final EventBus eventBus;
 
   @Inject
-  public PersonsResource(Dtos dtos, PersonService personService, EventBus eventBus) {
+  public PersonsResource(Dtos dtos, PersonService personService, SubjectAclService subjectAclService,
+    StudyService service, EventBus eventBus) {
     this.dtos = dtos;
     this.personService = personService;
+    this.subjectAclService = subjectAclService;
+    this.studyService = service;
     this.eventBus = eventBus;
   }
 
   @GET
   @Path("/study/{studyId}")
   public Set<PersonDto> getStudyMemberships(@PathParam("studyId") String studyId) {
+    if (studyService.isCollectionStudy(studyId)) {
+      subjectAclService.checkPermission("/draft/individual-study", "VIEW", studyId);
+    } else {
+      subjectAclService.checkPermission("/draft/harmonization-study", "VIEW", studyId);
+    }
     return personService.getStudyMemberships(studyId).stream().map(member -> dtos.asDto(member, true)).collect(Collectors.toSet());
   }
 
   @GET
   @Path("/network/{networkId}")
   public Set<PersonDto> getNetworkMemberships(@PathParam("networkId") String networkId) {
+    subjectAclService.checkPermission("/draft/network", "VIEW", networkId);
     return personService.getNetworkMemberships(networkId).stream().map(member -> dtos.asDto(member, true)).collect(Collectors.toSet());
   }
 
   @PUT
   @Path("/_index")
+  @RequiresPermissions({ "/draft/individual-study:EDIT", "/draft/harmonization-study:EDIT", "/draft/network:EDIT" })
   public Response index() {
     eventBus.post(new IndexContactsEvent());
     return Response.noContent().build();
   }
 
   @POST
+  @RequiresPermissions({ "/draft/individual-study:EDIT", "/draft/harmonization-study:EDIT", "/draft/network:EDIT" })
   public PersonDto createPerson(PersonDto personDto) {
     if (personDto == null) {
       return null;
