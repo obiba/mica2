f863cace420f1c47bc096933c361c8789a0e7f3b
Yannick Marcon <yannick.marcon@obiba.org>
2022-05-05 09:44:18 +0200

    csrf.allowed configuration property added

--- mica-rest/src/main/java/org/obiba/mica/web/rest/security/CSRFInterceptor.java
+++ mica-rest/src/main/java/org/obiba/mica/web/rest/security/CSRFInterceptor.java
@@ -10,6 +10,9 @@
 
 package org.obiba.mica.web.rest.security;
 
+import com.google.common.base.Splitter;
+import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,6 +20,8 @@ import javax.ws.rs.ForbiddenException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import java.io.IOException;
+import java.net.URI;
+import java.util.List;
 
 public class CSRFInterceptor implements ContainerRequestFilter {
 
@@ -28,26 +33,47 @@ public class CSRFInterceptor implements ContainerRequestFilter {
 
   private final String serverPort;
 
-  public CSRFInterceptor(String port) {
+  private final boolean productionMode;
+
+  private final List<String> csrfAllowed;
+
+  public CSRFInterceptor(String port, boolean productionMode, String csrfAllowed) {
     serverPort = port;
+    this.productionMode = productionMode;
+    this.csrfAllowed = Strings.isNullOrEmpty(csrfAllowed) ? Lists.newArrayList() : Splitter.on(",").splitToList(csrfAllowed.trim());
   }
 
   @Override
   public void filter(ContainerRequestContext requestContext)
     throws IOException {
+    if (!productionMode || csrfAllowed.contains("*")) return;
+
     String host = requestContext.getHeaderString(HOST_HEADER);
     String referer = requestContext.getHeaderString(REFERER_HEADER);
     if (referer != null) {
-      String localhost = String.format("localhost:%s", serverPort);
-      boolean forbidden = false;
+      String refererHostPort = "";
+      try {
+        URI refererURI = URI.create(referer);
+        refererHostPort = refererURI.getHost() + (refererURI.getPort() > 0 ? ":" + refererURI.getPort() : "");
+      } catch (Exception e) {
+        // malformed url
+      }
+      // explicitly ok
+      if (csrfAllowed.contains(refererHostPort)) return;
 
-      if (localhost.equals(host)) {
-        if (!referer.startsWith(String.format("http://%s/", localhost)))
+      String localhost = String.format("localhost:%s", serverPort);
+      String loopbackhost = String.format("127.0.0.1:%s", serverPort);
+      boolean forbidden = false;
+      if (localhost.equals(host) || loopbackhost.equals(host)) {
+        if (!referer.startsWith(String.format("http://%s/", host)))
           forbidden = true;
-      } else if (!referer.startsWith(String.format("https://%s/", host)))
+      } else if (!referer.startsWith(String.format("https://%s/", host))) {
         forbidden = true;
+      }
+
       if (forbidden) {
         log.warn("CSRF detection: Host={}, Referer={}", host, referer);
+        log.info(">> You can add {} to csrf.allowed setting", refererHostPort);
         throw new ForbiddenException();
       }
     }
