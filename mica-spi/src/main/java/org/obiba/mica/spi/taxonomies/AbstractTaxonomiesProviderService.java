1a422941cecc0950eec73d6ef42a7de81a38c0eb
Yannick Marcon <yannick.marcon@obiba.org>
2022-12-13 11:24:37 +0100

    AbstractTaxonomiesProviderService added

---
+++ mica-spi/src/main/java/org/obiba/mica/spi/taxonomies/AbstractTaxonomiesProviderService.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2022 OBiBa. All rights reserved.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.obiba.mica.spi.taxonomies;
+
+import java.util.Properties;
+
+public abstract class AbstractTaxonomiesProviderService implements TaxonomiesProviderService {
+
+  protected Properties properties;
+
+  protected boolean running;
+
+  @Override
+  public Properties getProperties() {
+    return properties;
+  }
+
+  @Override
+  public void configure(Properties properties) {
+    this.properties = properties;
+  }
+
+  @Override
+  public boolean isRunning() {
+    return running;
+  }
+
+  @Override
+  public void start() {
+    this.running = true;
+  }
+
+  @Override
+  public void stop() {
+    this.running = false;
+  }
+}
