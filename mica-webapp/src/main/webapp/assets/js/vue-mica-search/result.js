f110dab915898e7aa6d1d9113954c1082bc79803
meek0 <cedricgfont@gmail.com>
2023-04-26 09:35:42 -0400

    graphic result table percentage with defined width of 4rem

--- mica-webapp/src/main/webapp/assets/js/vue-mica-search/result.js
+++ mica-webapp/src/main/webapp/assets/js/vue-mica-search/result.js
@@ -278,16 +278,16 @@ const GraphicResult = {
 
                   <td class="col" v-bind:title="totals ? (100 * row.count/totals.countTotal).toFixed(2) + '%' : ''" v-if="row.count > 0">
                     <a href="" v-on:click="onCountClick($event, row.vocabulary, row.key, row.queryOverride)" class="query-anchor">{{row.count}}</a>
-                    <small class="ml-1" v-if="chartDataset.options.withTotals && chartDataset.options.withPercentages">({{totals ? (100 * row.count/totals.countTotal).toFixed(2) + '%' : ''}})</small>
+                    <small class="ml-1 d-inline-block" style="width: 4rem;" v-if="chartDataset.options.withTotals && chartDataset.options.withPercentages">({{totals ? (100 * row.count/totals.countTotal).toFixed(2) + '%' : ''}})</small>
                   </td>
 
                   <td class="col" v-bind:title="totals ? (0).toFixed(2) + '%' : ''" v-if="row.count === 0">
                     <span class="text-muted">{{row.count}}</span>
-                    <small v-if="chartDataset.options.withTotals && chartDataset.options.withPercentages" class="ml-1 text-muted">({{totals ? (0).toFixed(2) + '%' : ''}})</small>
+                    <small v-if="chartDataset.options.withTotals && chartDataset.options.withPercentages" class="ml-1 text-muted d-inline-block" style="width: 4rem;">({{totals ? (0).toFixed(2) + '%' : ''}})</small>
                   </td>
 
                   <td class="col" v-bind:title="totals ? (100 * row.subAgg/totals.subAggTotal).toFixed(2) + '%' : ''" v-if="row.subAgg !== undefined">
-                    <span v-bind:class="{ 'text-muted': row.subAgg !== undefined && row.subAgg === 0 }">{{row.subAgg !== undefined && row.subAgg === 0 ? '-' : row.subAgg.toLocaleString()}}</span>
+                    <span class="d-inline-block" style="width: 4rem;" v-bind:class="{ 'text-muted': row.subAgg !== undefined && row.subAgg === 0 }">{{row.subAgg !== undefined && row.subAgg === 0 ? '-' : row.subAgg.toLocaleString()}}</span>
                   </td>
                 </tr>
             </tbody>
@@ -296,7 +296,7 @@ const GraphicResult = {
                   <th class="col">{{ 'graphics.total' | translate }}</th>
                   <th class="col">
                     <span>{{totals.countTotal.toLocaleString()}}</span>
-                    <small class="ml-1" v-if="chartDataset.options.withTotals && chartDataset.options.withPercentages">({{(100).toFixed(2) + '%'}})</small>
+                    <small class="ml-1 d-inline-block" style="width: 4rem;" v-if="chartDataset.options.withTotals && chartDataset.options.withPercentages">({{(100).toFixed(2) + '%'}})</small>
                   </th>
                   <th class="col" v-if="totals.subAggTotal !== undefined">{{totals.subAggTotal.toLocaleString()}}</th>
                 </tr>
