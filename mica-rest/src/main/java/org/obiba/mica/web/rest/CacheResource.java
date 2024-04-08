3ef6a62223fc937e6f3704229f74a846a56c111a
Ramin Haeri Azad <rhaeri@maelstrom-reseach.org>
2024-04-08 10:59:31 -0400

    Fixed CacheResource and changed logback version

--- mica-rest/src/main/java/org/obiba/mica/web/rest/CacheResource.java
+++ mica-rest/src/main/java/org/obiba/mica/web/rest/CacheResource.java
@@ -22,7 +22,9 @@ import org.obiba.mica.micaConfig.service.CacheService;
 import org.obiba.mica.security.Roles;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+@Component
 @Path("/")
 @RequiresRoles(Roles.MICA_ADMIN)
 public class CacheResource {
