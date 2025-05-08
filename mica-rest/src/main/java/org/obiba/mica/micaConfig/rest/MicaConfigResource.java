d2708476566d849c8423df1a42628d10d37ac239
Yannick Marcon <yannick.marcon@obiba.org>
2025-05-08 17:34:42 +0200

    fix: restored auth required to get config from rest api

--- mica-rest/src/main/java/org/obiba/mica/micaConfig/rest/MicaConfigResource.java
+++ mica-rest/src/main/java/org/obiba/mica/micaConfig/rest/MicaConfigResource.java
@@ -126,7 +126,7 @@ public class MicaConfigResource {
 
   @GET
   @Timed
-  //@RequiresAuthentication
+  @RequiresAuthentication
   public Mica.MicaConfigDto get() {
     return dtos.asDto(micaConfigService.getConfig());
   }
