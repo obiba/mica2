b76dbcfa6b0dfdbc940fa02fa7c523be36ee15b1
Yannick Marcon <yannick.marcon@obiba.org>
2026-05-10 20:09:27 +0200

    feat: added mica-ui

---
+++ mica-ui/src/components/ConfirmDialog.vue
@@ -0,0 +1,56 @@
+<template>
+  <q-dialog v-model="showDialog" persistent @hide="onHide">
+    <q-card class="dialog-sm">
+      <q-card-section>
+        <div class="text-h6">{{ props.title }}</div>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-section>
+        <span>{{ props.text }}</span>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions align="right" class="bg-grey-3">
+        <q-btn flat :label="t('cancel')" color="secondary" @click="onCancel" v-close-popup />
+        <q-btn flat :label="t('confirm')" color="primary" @click="onConfirm" v-close-popup />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup lang="ts">
+const { t } = useI18n();
+
+interface DialogProps {
+  modelValue: boolean;
+  title: string;
+  text: string;
+}
+
+const props = defineProps<DialogProps>();
+const emit = defineEmits(['update:modelValue', 'confirm', 'cancel']);
+
+const showDialog = ref(props.modelValue);
+
+watch(
+  () => props.modelValue,
+  (value) => {
+    showDialog.value = value;
+  },
+);
+
+function onHide() {
+  emit('update:modelValue', false);
+}
+
+function onCancel() {
+  emit('cancel', true);
+}
+
+function onConfirm() {
+  emit('confirm', true);
+}
+</script>
---
+++ mica-ui/src/pages/IndexPage.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <q-toolbar class="bg-grey-3">
+      <q-breadcrumbs>
+        <q-breadcrumbs-el icon="home" />
+      </q-breadcrumbs>
+    </q-toolbar>
+    <q-page class="q-pa-md">
+      <div class="row q-col-gutter-lg q-mb-lg">
+        <div class="col-8 col-sm-8 col-xs-12">
+          <q-item-label header class="text-uppercase">{{ t('content_management') }}</q-item-label>
+          <div class="row">
+            <q-card flat bordered class="on-left q-mb-md o-card-sm bg-grey-1">
+              <q-card-section class="q-pa-sm text-h6 text-center bg-grey-4">
+                <router-link to="/networks">{{ t('networks') }}</router-link>
+              </q-card-section>
+              <q-separator />
+              <q-card-section class="text-hint">
+                {{ t('networks_caption') }}
+                <q-btn flat rounded dense icon="arrow_forward" size="xs" color="primary" :to="`/networks`" />
+              </q-card-section>
+            </q-card>
+            <q-card flat bordered class="on-left q-mb-md o-card-sm bg-grey-1">
+              <q-card-section class="q-pa-sm text-h6 text-center bg-grey-4">
+                <router-link to="/studies">{{ t('studies') }}</router-link>
+              </q-card-section>
+              <q-separator />
+              <q-card-section class="text-hint">
+                {{ t('studies_caption') }}
+                <q-btn flat rounded dense icon="arrow_forward" size="xs" color="primary" :to="`/studies`" />
+              </q-card-section>
+            </q-card>
+            <q-card flat bordered class="on-left q-mb-md o-card-sm bg-grey-1">
+              <q-card-section class="q-pa-sm text-h6 text-center bg-grey-4">
+                <router-link to="/datasets">{{ t('datasets') }}</router-link>
+              </q-card-section>
+              <q-separator />
+              <q-card-section class="text-hint">
+                {{ t('datasets_caption') }}
+                <q-btn flat rounded dense icon="arrow_forward" size="xs" color="primary" :to="`/datasets`" />
+              </q-card-section>
+            </q-card>
+            <q-card flat bordered class="on-left q-mb-md o-card-sm bg-grey-1">
+              <q-card-section class="q-pa-sm text-h6 text-center bg-grey-4">
+                <router-link to="/research-projects">{{ t('research_projects') }}</router-link>
+              </q-card-section>
+              <q-separator />
+              <q-card-section class="text-hint">
+                {{ t('research_projects_caption') }}
+                <q-btn flat rounded dense icon="arrow_forward" size="xs" color="primary" :to="`/research-projects`" />
+              </q-card-section>
+            </q-card>
+            <q-card flat bordered class="on-left q-mb-md o-card-sm bg-grey-1">
+              <q-card-section class="q-pa-sm text-h6 text-center bg-grey-4">
+                <router-link to="/files">{{ t('files') }}</router-link>
+              </q-card-section>
+              <q-separator />
+              <q-card-section class="text-hint">
+                {{ t('files_caption') }}
+                <q-btn flat rounded dense icon="arrow_forward" size="xs" color="primary" :to="`/files`" />
+              </q-card-section>
+            </q-card>
+            <q-card flat bordered class="on-left q-mb-md o-card-sm bg-grey-1">
+              <q-card-section class="q-pa-sm text-h6 text-center bg-grey-4">
+                <router-link to="/persons">{{ t('persons') }}</router-link>
+              </q-card-section>
+              <q-separator />
+              <q-card-section class="text-hint">
+                {{ t('persons_caption') }}
+                <q-btn flat rounded dense icon="arrow_forward" size="xs" color="primary" :to="`/persons`" />
+              </q-card-section>
+            </q-card>
+          </div>
+        </div>
+        <div class="col-4 col-sm-4 col-xs-12"></div>
+      </div>
+    </q-page>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { t } = useI18n();
+//const authStore = useAuthStore();
+</script>
