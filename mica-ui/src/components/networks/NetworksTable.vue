5573e996c85ad4729990b016491c1b4a5a6fb4b5
Yannick Marcon <yannick.marcon@obiba.org>
2026-05-15 15:23:57 +0200

    feat: added networks page

---
+++ mica-ui/src/components/networks/NetworksTable.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <q-table
+            flat
+            :rows="networksStore.networks"
+            :columns="columns"
+            row-key="id"
+        >
+            <template v-slot:body-cell-name="props">
+            <q-td key="name" :props="props">
+                <div v-for="name in props.row.name" :key="name">
+                    <q-badge color="primary" class="q-mr-sm">{{ name.lang }}</q-badge>
+                    {{ name.value }}
+                </div>
+            </q-td>
+            </template>
+        </q-table>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { TimestampsDto } from 'src/models/Mica';
+
+const networksStore = useNetworksStore();
+const { t } = useI18n();
+
+const columns = computed(() => [
+  { name: 'id', label: 'ID', field: 'id', sortable: true },
+  { name: 'name', label: t('name'), field: 'name', sortable: true },
+  { name: 'lastUpdated', label: t('lastUpdated'), field: 'timestamps', format: (ts: TimestampsDto) => ts.lastUpdate, sortable: true },
+]);
+
+onMounted(() => {
+    networksStore.fetchNetworks();
+});
+</script>
